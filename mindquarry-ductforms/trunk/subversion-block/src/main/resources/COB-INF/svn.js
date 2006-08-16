93f00f2405933a76ef9c4d445dd017a03b08913e
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-08-16 12:27:31 +0000

    r1004@notebook:  lars | 2006-08-16 14:17:36 +0200  deleted cocoon playground

--- mindquarry-ductforms/trunk/subversion-block/src/main/resources/COB-INF/svn.js
+++
@@ -1,5 +0,0 @@
-function showRepository() {
-	var svnBean = cocoon.getComponent("svn-block");
-	cocoon.sendPage("repository-jx", { "url" : svnBean.repositoryUrl, 
-									"entries" : svnBean.repositoryEntries });
-}
