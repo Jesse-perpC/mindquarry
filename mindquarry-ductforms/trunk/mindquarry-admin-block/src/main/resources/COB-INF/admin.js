389415c0c8c25abae804ebaabf80f87bcd92763b
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-09-14 09:46:01 +0000

    r1247@mbp17:  lars | 2006-09-14 11:39:43 +0200  addition and deletion works

--- mindquarry-ductforms/trunk/mindquarry-admin-block/src/main/resources/COB-INF/admin.js
+++ mindquarry-ductforms/trunk/mindquarry-admin-block/src/main/resources/COB-INF/admin.js
@@ -1,13 +1,23 @@
 cocoon.load("resource://org/apache/cocoon/forms/flow/javascript/Form.js");
 
+var model_;
+
 function manageProjects(form) {
 
-	var model = form.getModel();
+	model_ = form.getModel();
 	
-	model.projects[0].name = "Foo";
-	model.projects[1].name = "Bar";
-	model.projects[2].name = "Hello World";
-	print(model.projects);
+	model_.projects[0].name = "Foo";
+	model_.projects[1].name = "Bar";
+	model_.projects[2].name = "Hello World";
 	
 	form.showForm("admin.instance");
+	
+	cocoon.redirectTo("", false);
+}
+
+function createProject(event) {
+	print(event.source.lookupWidget("../newprojectname").value);
+	
+	model_.projects[model_.projects.length - 1].name = event.source.lookupWidget("../newprojectname").value;
+	event.source.lookupWidget("../newprojectname").value = "";
 }
