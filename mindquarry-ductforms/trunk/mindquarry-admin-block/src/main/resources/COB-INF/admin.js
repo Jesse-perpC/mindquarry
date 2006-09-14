ab9b1194332c6daa6a4279017ecdd59efc58c18d
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-09-14 11:47:02 +0000

    r1251@mbp17:  lars | 2006-09-14 13:40:24 +0200  allow addition of projects

--- mindquarry-ductforms/trunk/mindquarry-admin-block/src/main/resources/COB-INF/admin.js
+++ mindquarry-ductforms/trunk/mindquarry-admin-block/src/main/resources/COB-INF/admin.js
@@ -1,23 +1,41 @@
 cocoon.load("resource://org/apache/cocoon/forms/flow/javascript/Form.js");
 
 var model_;
+var projectadmin_ = cocoon.getComponent("projectadmin");
 
 function manageProjects(form) {
-
 	model_ = form.getModel();
-	
-	model_.projects[0].name = "Foo";
-	model_.projects[1].name = "Bar";
-	model_.projects[2].name = "Hello World";
-	
+	populateModel();
 	form.showForm("admin.instance");
-	
 	cocoon.redirectTo("", false);
 }
 
+function populateModel() {
+	var list = projectadmin_.list();
+	for (var i=0;i<list.size();i++) {
+		model_.projects[i].name = list.get(i).name;
+	}
+}
+
+function isDuplicate(name) {
+	var list = projectadmin_.list();
+	for (var i=0;i<model_.projects.length;i++) {
+		if (model_.projects[i].name == name) {
+			return true;
+		}
+	}
+	return false;
+}
+
 function createProject(event) {
-	print(event.source.lookupWidget("../newprojectname").value);
-	
-	model_.projects[model_.projects.length - 1].name = event.source.lookupWidget("../newprojectname").value;
-	event.source.lookupWidget("../newprojectname").value = "";
+	var projectname = event.source.lookupWidget("../newprojectname").value;
+	if (! isDuplicate(projectname)) {
+		print("Creating project");
+		projectadmin_.create(projectname);
+		populateModel();
+		event.source.lookupWidget("../newprojectname").value = "";
+	} else {
+		print("duplicate");
+		return false;
+	}
 }
