9cd4e66c7c5865469d291beccadd31e7227f0ecf
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-09-15 18:04:44 +0000

    some hacking done

--- mindquarry-ductforms/trunk/jcr-excalibur-sourceresolver/src/main/java/org/apache/excalibur/source/impl/jcr/xml/JCRXMLNodeSource.java
+++ mindquarry-ductforms/trunk/jcr-excalibur-sourceresolver/src/main/java/org/apache/excalibur/source/impl/jcr/xml/JCRXMLNodeSource.java
@@ -1,82 +1,336 @@
-/**
- * 
+/*
+ * Copyright 2006 Mindquarry GmbH, Potsdam, Germany
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.excalibur.source.impl.jcr.xml;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.Item;
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
-import org.apache.excalibur.source.impl.jcr.JCRNodeSource;
-import org.apache.excalibur.source.impl.jcr.JCRSourceFactory;
+import org.apache.excalibur.source.ModifiableSource;
 import org.apache.excalibur.source.SourceException;
 import org.apache.excalibur.source.SourceNotFoundException;
+import org.apache.excalibur.source.SourceValidity;
 import org.apache.excalibur.xml.sax.XMLizable;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
 /**
  * A source for an XML-izable JCR Node.
- *
+ * 
  */
-public class JCRXMLNodeSource extends JCRNodeSource implements XMLizable {
+public class JCRXMLNodeSource implements ModifiableSource, XMLizable {
 
-	public JCRXMLNodeSource(JCRSourceFactory factory, Session session,
-			String path) throws SourceException {
-		super(factory, session, path);
-	}
+    /** The full URI */
+    protected String computedURI;
 
-	public JCRXMLNodeSource(JCRSourceFactory factory, Node node)
-			throws SourceException {
-		super(factory, node);
-	}
+    /** The node path */
+    protected final String path;
 
-	public JCRXMLNodeSource(JCRNodeSource parent, Node node)
-			throws SourceException {
-		super(parent, node);
-	}
+    /** The factory that created this Source */
+    protected final JCRXMLSourceFactory factory;
 
-	public void toSAX(ContentHandler handler) throws SAXException {
-		// use the JCR export feature
-		try {
-			this.session.exportDocumentView(this.path, handler, false, false);
-		} catch (PathNotFoundException e) {
+    /** The session this source is bound to */
+    protected final Session session;
+
+    /** The node pointed to by this source (can be null) */
+    protected Node node;
+
+    public JCRXMLNodeSource(JCRXMLSourceFactory factory, Session session,
+            String path) throws SourceException {
+        this.factory = factory;
+        this.session = session;
+        this.path = path;
+
+        try {
+            Item item = session.getItem(path);
+            if (!item.isNode()) {
+                throw new SourceException("Path '" + path
+                        + "' is a property (should be a node)");
+            } else {
+                this.node = (Node) item;
+            }
+        } catch (PathNotFoundException e) {
+            // Not found
+            this.node = null;
+        } catch (RepositoryException e) {
+            throw new SourceException("Cannot lookup repository path " + path,
+                    e);
+        }
+    }
+
+    public JCRXMLNodeSource(JCRXMLSourceFactory parent, Node node)
+            throws SourceException {
+        this.factory = parent;
+        this.node = node;
+
+        try {
+            this.session = node.getSession();
+            this.path = node.getPath();
+        } catch (RepositoryException e) {
+            throw new SourceException("Cannot get node's informations", e);
+        }
+    }
+
+    public JCRXMLNodeSource(JCRXMLNodeSource parent, Node node)
+            throws SourceException {
+        this.factory = parent.factory;
+        this.session = parent.session;
+        this.node = node;
+
+        try {
+            this.path = getChildPath(parent.path, node.getName());
+
+        } catch (RepositoryException e) {
+            throw new SourceException("Cannot get name of child of "
+                    + parent.getURI(), e);
+        }
+    }
+
+    // =========================================================================
+    // Source interface
+    // =========================================================================
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.cocoon.jcr.source.JCRNodeSource#getInputStream()
+     */
+    public InputStream getInputStream() throws IOException,
+            SourceNotFoundException {
+        // used when the source is non-xml, aka text or binary
+        // return super.getInputStream();
+        return null;
+    }
+
+    public boolean exists() {
+        return this.node != null;
+    }
+
+    public long getContentLength() {
+        // TODO: if content length is known because it is a binary/text
+        // content return it here
+        
+        // we don't know the content length due to dynamic content of xml subtree
+        return -1;
+    }
+
+    public long getLastModified() {
+        // TODO: getLastModified()
+
+        // example code for setting the last modified property
+//      contentNode.setProperty(info.lastModifiedProp, new GregorianCalendar());
+
+        return 0;
+    }
+
+    public String getMimeType() {
+        if (!exists()) {
+            return null;
+        }
+        // TODO: getMimeType(): use some node property
+        return null;
+//        try {
+//            Property prop = this.factory.getMimeTypeProperty(this.node);
+//            if (prop == null) {
+//                return null;
+//            } else {
+//                String value = prop.getString();
+//                return value.length() == 0 ? null : value;
+//            }
+//        } catch (RepositoryException re) {
+//            return null;
+//        }
+    }
+
+    public String getScheme() {
+        return this.factory.getScheme();
+    }
+
+    public String getURI() {
+        if (this.computedURI == null) {
+            this.computedURI = this.factory.getScheme() + ":/" + this.path;
+        }
+        return this.computedURI;
+    }
+
+    public SourceValidity getValidity() {
+        if (!exists()) {
+            return null;
+        }
+        // TODO: getValidity()
+        return null;
+//        try {
+//            Property prop = this.factory.getValidityProperty(this.node);
+//            return prop == null ? null : new JCRNodeSourceValidity(prop.getValue());
+//        } catch (RepositoryException re) {
+//            return null;
+//        }
+    }
+
+    public void refresh() {
+        // nothing to do here
+    }
+
+    // =========================================================================
+    // XMLizable interface
+    // =========================================================================
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.excalibur.xml.sax.XMLizable#toSAX()
+     */
+    public void toSAX(ContentHandler handler) throws SAXException {
+        // use the JCR export feature
+        try {
+            this.session.exportDocumentView(this.path, handler, false, false);
+        } catch (PathNotFoundException e) {
             throw new SAXException("Source not found: " + this.getURI());
         } catch (RepositoryException e) {
-            throw new SAXException("Cannot export repository path as document view sax events: " + this.path, e);
-		}
-	}
+            throw new SAXException(
+                    "Cannot export repository path as document view sax events: "
+                            + this.path, e);
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.apache.cocoon.jcr.source.JCRNodeSource#getInputStream()
-	 */
-	public InputStream getInputStream() throws IOException, SourceNotFoundException {
-		// TODO Auto-generated method stub
-		return super.getInputStream();
-	}
+    // =========================================================================
+    // ModifiableSource interface
+    // =========================================================================
 
-	/* (non-Javadoc)
-	 * @see org.apache.cocoon.jcr.source.JCRNodeSource#getOutputStream()
-	 */
-	public OutputStream getOutputStream() throws IOException {
-		// TODO Auto-generated method stub
-		// if this node is a leaf node with direct content,
-		//   write the content directly as in the subclass into
-		//   some content property of the node (see JCRSourceOutputStream)
-		// else if this node has no children yet, parse xml and create substructure
-		//
-		// ??? How to overwrite existing XML structure ???
-		// a) structure the same, different content
-		// b) only a part
-		// c) with additional elements
-		// d) mixed up
-		return super.getOutputStream();
-	}
+    /**
+     * Write into this source, can be XML.
+     */
+    public OutputStream getOutputStream() throws IOException {
+        // if this node is a leaf node with direct content,
+        // write the content directly as in the subclass into
+        // some content property of the node (see JCRSourceOutputStream)
+        // else if this node has no children yet, parse xml and create
+        // substructure
+        //
+        // ??? How to overwrite existing XML structure ???
+        // a) structure the same, different content
+        // b) only a part
+        // c) with additional elements
+        // d) mixed up
 
-	
+        return new JCRXMLSourceOutputStream(this.path);
+    }
+
+    public boolean canCancel(OutputStream stream) {
+        return false;
+//        if (os instanceof JCRSourceOutputStream) {
+//            return ((JCRSourceOutputStream) os).canCancel();
+//        } else {
+//            return false;
+//        }
+    }
+
+    public void cancel(OutputStream stream) throws IOException {
+//        if (canCancel(os)) {
+//            ((JCRSourceOutputStream) os).cancel();
+//        } else {
+//            throw new IllegalArgumentException("Stream cannot be cancelled");
+//        }
+    }
+
+    public void delete() throws SourceException {
+        if (exists()) {
+            try {
+                this.node.remove();
+                this.node = null;
+                this.session.save();
+            } catch (RepositoryException e) {
+                throw new SourceException("Cannot delete " + getURI(), e);
+            }
+        }
+    }
+
+    // =========================================================================
+    // Internal methods / classes
+    // =========================================================================
+
+    /**
+     * Utility method that calculates the correct child path.
+     */
+    private String getChildPath(String path, String name) {
+        StringBuffer pathBuf = new StringBuffer(path);
+        // Append '/' only if the parent isn't the root (it's path is "/" in
+        // that case)
+        if (pathBuf.length() > 1)
+            pathBuf.append('/');
+        pathBuf.append(name);
+        return pathBuf.toString();
+    }
+
+    /**
+     * An outputStream that will import the stream as XML upon close, and
+     * discard it upon cancel.
+     */
+    private class JCRXMLSourceOutputStream extends ByteArrayOutputStream {
+        private boolean isClosed = false;
+
+        private final String parentAbsPath;
+
+        public JCRXMLSourceOutputStream(String parentAbsPath) {
+            this.parentAbsPath = parentAbsPath;
+        }
+
+        public void close() throws IOException {
+            if (isClosed) {
+                return;
+            }
+
+            super.close();
+            this.isClosed = true;
+
+            // SAXParser
+            try {
+                JCRXMLNodeSource.this.session
+                        .getWorkspace()
+                        .importXML(
+                                this.parentAbsPath,
+                                new ByteArrayInputStream(this.toByteArray()),
+                                ImportUUIDBehavior.IMPORT_UUID_COLLISION_REMOVE_EXISTING);
+            } catch (RepositoryException e) {
+                IOException ioe = new IOException("Cannot save content to "
+                        + getURI());
+                ioe.initCause(e);
+                throw ioe;
+            }
+        }
+
+        public boolean canCancel() {
+            return !isClosed;
+        }
+
+        public void cancel() throws IOException {
+            if (isClosed) {
+                throw new IllegalStateException(
+                        "Cannot cancel : outputstrem is already closed");
+            }
+        }
+    }
 }
--- mindquarry-ductforms/trunk/jcr-excalibur-sourceresolver/src/main/java/org/apache/excalibur/source/impl/jcr/xml/JCRXMLSourceFactory.java
+++ mindquarry-ductforms/trunk/jcr-excalibur-sourceresolver/src/main/java/org/apache/excalibur/source/impl/jcr/xml/JCRXMLSourceFactory.java
@@ -1,34 +1,123 @@
-/**
- * 
+/*
+ * Copyright 2006 Mindquarry GmbH, Potsdam, Germany
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.excalibur.source.impl.jcr.xml;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashSet;
+import java.util.Map;
 
+import javax.jcr.LoginException;
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
 
+import org.apache.avalon.framework.CascadingRuntimeException;
+import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.excalibur.source.impl.jcr.JCRNodeSource;
-import org.apache.excalibur.source.impl.jcr.JCRSourceFactory;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.avalon.framework.thread.ThreadSafe;
+import org.apache.excalibur.source.Source;
 import org.apache.excalibur.source.SourceException;
+import org.apache.excalibur.source.SourceFactory;
+import org.apache.excalibur.source.SourceUtil;
 
 /**
- * This implementation extends <code>JCRSourceFactory</code> to provide
+ * This implementation extends <code>JCRSourceFactory</code> to provide an
  * XML-izable <code>JCRXMLNodeSource</code>s.
- *
+ * 
+ * <p>
+ * An URI for this source is either (i) a direct path in the repository or (ii)
+ * an enclosed JCR query. The path to the repository would be given simply as
+ * <code>jcr://root/folder/file</code>. For the queries, three different
+ * possibilities exist:
+ * <ul>
+ * <li>XPATH: <code>jcr://xpath!//folder/file</code> (which maps to the xpath
+ * query <code>//folder/file</code>)</li>
+ * <li>XPATH: <code>jcr://!//folder/file</code> (shorthand notation, same
+ * query as above)</li>
+ * <li>SQL:
+ * <code>jcr://sql!select * from nt:base where jcr:path = '/root/myfile'</code>
+ * (well, which maps to the sql query
+ * <code>SELECT * FROM nt:base WHERE jcr:path = '/root/myfile'</code>)</li>
+ * </ul>
+ * Note that this implementation does only support queries that return a
+ * content-node (with no children). If the result contains multiple nodes, the
+ * first one is used. See also the
+ * {@link #executeQuery(Session,String,String) executeQuery()} method.
+ * 
  */
-public class JCRXMLSourceFactory extends JCRSourceFactory {
+public class JCRXMLSourceFactory implements ThreadSafe, SourceFactory,
+        Configurable, Serviceable {
 
-	/**
-	 * A hash containing all XML elements that should be versionized
-	 */
-	protected HashSet versionizedElements;
-	
-	public void configure(Configuration config) throws ConfigurationException {
-		this.versionizedElements = new HashSet();
-		
+    /**
+     * A hash containing all XML elements that should be versionized
+     */
+    protected HashSet versionizedElements;
+
+    /**
+     * The reference to the JCR Repository to use as interface.
+     */
+    protected Repository repo;
+
+    /**
+     * Scheme, lazily computed at the first call to getSource()
+     */
+    protected String scheme;
+
+    /**
+     * Used to resolve other components
+     */
+    protected ServiceManager manager;
+
+    // =========================================================================
+    // Servicable interface
+    // =========================================================================
+
+    /**
+     * Called at startup of this component.
+     */
+    public void service(ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+        // this.repo is lazily initialized to avoid a circular dependency
+        // between SourceResolver and JackrabbitRepository that leads to a
+        // StackOverflowError at initialization time
+    }
+
+    // =========================================================================
+    // Configurable interface
+    // =========================================================================
+
+    /**
+     * Configures this component based on some <code>Configuration</code> read
+     * from an XML config file. The configurable elements should be defined in
+     * the class javadoc comment!
+     */
+    public void configure(Configuration config) throws ConfigurationException {
+        this.versionizedElements = new HashSet();
+
         Configuration[] children = config.getChildren();
 
         for (int i = 0; i < children.length; i++) {
@@ -36,25 +125,180 @@ public class JCRXMLSourceFactory extends JCRSourceFactory {
             String name = child.getName();
 
             if ("versionize".equals(name)) {
-            	this.versionizedElements.add(child.getAttribute("element"));
+                this.versionizedElements.add(child.getAttribute("element"));
             }
         }
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see org.apache.cocoon.jcr.source.JCRSourceFactory#createSource(org.apache.cocoon.jcr.source.JCRNodeSource, javax.jcr.Node)
-	 */
-	public JCRNodeSource createSource(JCRNodeSource parent, Node node) throws SourceException {
-		return new JCRXMLNodeSource(parent, node);
-	}
+    // =========================================================================
+    // SourceFactory interface
+    // =========================================================================
 
-	/* (non-Javadoc)
-	 * @see org.apache.cocoon.jcr.source.JCRSourceFactory#createSource(javax.jcr.Session, java.lang.String)
-	 */
-	public JCRNodeSource createSource(Session session, String path) throws SourceException {
-		return new JCRXMLNodeSource(this, session, path);
-	}
-	
-	
+    /**
+     * Retrieves a <code>Source</code> for the given uri. The URI can also be
+     * an Xpath or SQL query conforming to the JCR query support.
+     */
+    public Source getSource(String uri, Map parameters) throws IOException,
+            MalformedURLException {
+        lazyInit();
 
+        if (this.scheme == null) {
+            this.scheme = SourceUtil.getScheme(uri);
+        }
+
+        Session session;
+        try {
+            // TODO: accept a different workspace?
+            session = repo.login();
+        } catch (LoginException e) {
+            throw new SourceException("Login to repository failed", e);
+        } catch (RepositoryException e) {
+            throw new SourceException("Cannot access repository", e);
+        }
+
+        // Compute the path
+        String path = SourceUtil.getSpecificPart(uri);
+        if (!path.startsWith("//")) {
+            throw new MalformedURLException("Expecting " + this.scheme
+                    + "://path and got " + uri);
+        }
+
+        // check for query syntax (eg. 'jcr://xpath!//element(*, nt:folder)' )
+
+        if (path.startsWith("//!")) {
+            return executeQuery(session, path.substring(3), Query.XPATH);
+        } else if (path.startsWith("//xpath!")) {
+            return executeQuery(session, path.substring(8), Query.XPATH);
+        } else if (path.startsWith("//sql!")) {
+            return executeQuery(session, path.substring(6), Query.SQL);
+        }
+
+        // standard direct hierarchy-resolving
+
+        // Remove first '/'
+        path = path.substring(1);
+        int pathLen = path.length();
+        if (pathLen > 1) {
+            // Not root: ensure there's no trailing '/'
+            if (path.charAt(pathLen - 1) == '/') {
+                path = path.substring(0, pathLen - 1);
+            }
+        }
+
+        return createSource(session, path);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.excalibur.source.SourceFactory#release(org.apache.excalibur.source.Source)
+     */
+    public void release(Source source) {
+        // nothing to do here
+    }
+
+    // =========================================================================
+    // Custom public interface
+    // =========================================================================
+
+    /**
+     * Creates a new source given its parent and its node
+     * 
+     * @param parent the parent
+     * @param node the node
+     * @return a new source
+     * @throws SourceException
+     */
+    public JCRXMLNodeSource createSource(JCRXMLNodeSource parent, Node node)
+            throws SourceException {
+        return new JCRXMLNodeSource(parent, node);
+    }
+
+    /**
+     * Creates a new source given a session and a path
+     * 
+     * @param session the session
+     * @param path the absolute path
+     * @return a new source
+     * @throws SourceException
+     */
+    public JCRXMLNodeSource createSource(Session session, String path)
+            throws SourceException {
+        return new JCRXMLNodeSource(this, session, path);
+    }
+
+    // =========================================================================
+    // Internal methods
+    // =========================================================================
+
+    /**
+     * Retrieves the reference of the JCR <code>Repository</code> to use.
+     * 
+     */
+    protected void lazyInit() {
+        // check if already initialized
+        if (this.repo != null) {
+            return;
+        }
+
+        try {
+            this.repo = (Repository) manager.lookup(Repository.class.getName());
+        } catch (Exception e) {
+            throw new CascadingRuntimeException("Cannot lookup repository", e);
+        }
+    }
+
+    /**
+     * Executes an XPath Query on the JCR repository. This implementation only
+     * allows queries that return one node with no children (because
+     * JCRNodeSource is a simple, non-xmlizable Source, that does not know what
+     * to do if someone wants to read (call getInputStream()) from a collection
+     * (or folder-type) node). If the result contains multiple nodes, only the
+     * first one is returned.
+     * 
+     * <p>
+     * Subclasses that use an extended, XMLizable JCRNodeSource should override
+     * this method and return all nodes in the result.
+     * 
+     * @param session the session
+     * @param statement the Xpath query statement
+     * @param queryLang the language to use (should be Query.SQL or Query.XPATH)
+     * @throws IOException when the query is wrong or the result was empty
+     */
+    protected Source executeQuery(Session session, String statement,
+            String queryLang) throws IOException {
+        if (queryLang == Query.SQL
+                && !session.getRepository().getDescriptor(
+                        Repository.OPTION_QUERY_SQL_SUPPORTED).equals("true")) {
+            throw new SourceException(
+                    "JCR Repository does not support SQL queries.");
+        }
+
+        try {
+            QueryManager queryManager = session.getWorkspace()
+                    .getQueryManager();
+            Query query = queryManager.createQuery(statement, queryLang);
+            QueryResult result = query.execute();
+
+            NodeIterator nodeIter = result.getNodes();
+
+            if (nodeIter.getSize() == 0) {
+                return null;
+            }
+
+            // return only the first result.
+            return new JCRXMLNodeSource(this, nodeIter.nextNode());
+        } catch (RepositoryException e) {
+            throw new SourceException("Cannot execute query '" + statement
+                    + "'", e);
+        }
+    }
+
+    /**
+     * Returns the scheme (probably <code>jcr</code>) for the URIs we handle.
+     * 
+     */
+    public String getScheme() {
+        return this.scheme;
+    }
 }
