c34850b3735c49f265745185f9682ec8e736d7f0
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-09-19 15:13:08 +0000

    - refactored handler namespace - added test skeletons for each source type

---
+++ trunk/mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/jcr/source/xml/handler/JCRNodesToSAXConverter.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2006 MindQuarry GmbH, All Rights Reserved
+ */
+package com.mindquarry.jcr.source.xml.handler;
+
+/**
+ * Add summary documentation here.
+ *
+ * @author <a href="mailto:alexander(dot)saar(at)mindquarry(dot)com">Alexander
+ *         Saar</a>
+ */
+public class JCRNodesToSAXConverter {
+
+}
