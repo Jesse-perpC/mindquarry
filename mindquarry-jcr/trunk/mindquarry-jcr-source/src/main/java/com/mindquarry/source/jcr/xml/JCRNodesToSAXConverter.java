8a4a2353d64b5fb985254032e5c888e8818fd19d
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-09-19 13:37:51 +0000

    added mindquarry-jcr teamspace

---
+++ mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/source/jcr/xml/JCRNodesToSAXConverter.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2006 Mindquarry GmbH, Potsdam, Germany
+ */
+package com.mindquarry.source.jcr.xml;
+
+/**
+ * @author alexander.klimetschek@mindquarry.com
+ *
+ */
+public class JCRNodesToSAXConverter {
+
+}
---
+++ mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/source/jcr/xml/sources/JCRNodeSourceValidity.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2006 Mindquarry GmbH
+ */
+package com.mindquarry.source.jcr.xml.sources;
+
+import javax.jcr.Value;
+
+import org.apache.excalibur.source.SourceValidity;
+
+/**
+ * Validity of a {@link JCRNodeSource}. It's a wrapper around a JCR
+ * <code>Value</code>.
+ *
+ * @author alexander.klimetschek@mindquarry.com
+ */
+public class JCRNodeSourceValidity implements SourceValidity {
+
+    private static final long serialVersionUID = 1L;
+
+    private Value value;
+
+    public JCRNodeSourceValidity(Value value) {
+        this.value = value;
+    }
+
+    public int isValid() {
+        // Don't know, need another validity to compare with
+        return 0;
+    }
+
+    public int isValid(SourceValidity other) {
+        if (other instanceof JCRNodeSourceValidity) {
+            // compare the two values
+            return ((JCRNodeSourceValidity) other).value.equals(this.value) ? 1 : -1;
+        } else {
+            // invalid
+            return -1;
+        }
+    }
+}
