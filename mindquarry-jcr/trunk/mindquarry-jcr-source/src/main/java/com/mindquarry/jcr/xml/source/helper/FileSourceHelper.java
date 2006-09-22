35c802db791f9f22d415f33f3a006e5a56ca9477
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-09-22 13:32:07 +0000

    jcr

--- mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/jcr/xml/source/helper/FileSourceHelper.java
+++ mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/jcr/xml/source/helper/FileSourceHelper.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
@@ -22,6 +23,7 @@ import org.apache.excalibur.source.SourceNotFoundException;
 
 import com.mindquarry.jcr.xml.source.AbstractJCRNodeSource;
 import com.mindquarry.jcr.xml.source.JCRSourceFactory;
+import com.mindquarry.jcr.xml.source.helper.stream.XMLFileOutputStream;
 
 /**
  * Source for a node that represents a file (nt:file) or a folder (nt:folder).
@@ -32,41 +34,27 @@ import com.mindquarry.jcr.xml.source.JCRSourceFactory;
  * @author <a href="mailto:alexander(dot)saar(at)mindquarry(dot)com">Alexander
  *         Saar</a>
  */
-public class FileSourceHelper extends AbstractJCRNodeSource implements
-        ModifiableTraversableSource {
-    /**
-     * Default contructor. Passes all parameters to the constructor of the super
-     * class.
-     * 
-     * {@link AbstractJCRNodeSource#AbstractJCRNodeSource(JCRSourceFactory, Session, String)}
-     */
-    public FileSourceHelper(JCRSourceFactory factory, Session session,
-            String path) throws SourceException {
-        super(factory, session, path);
-    }
-
+public class FileSourceHelper {
     // =========================================================================
     // Source interface
     // =========================================================================
 
     /**
+     * @param node 
      * @see org.apache.excalibur.source.Source#getInputStream()
      */
-    public InputStream getInputStream() throws IOException,
+    public static InputStream getInputStream(Node node) throws IOException,
             SourceNotFoundException {
-        return null;
+    	try {
+			Node content = node.getNode("jcr:content");
+			return content.getProperty("jcr:data").getStream();
+		} catch (PathNotFoundException e) {
+			throw new SourceNotFoundException("No valid jcr:data property", e);
+		} catch (RepositoryException e) {
+			throw new IOException("Unable to read from repository: " + e.getLocalizedMessage());
+		}
     }
 
-    // =========================================================================
-    // ModifiableTraversableSource interface
-    // =========================================================================
-
-    /**
-     * @see org.apache.excalibur.source.ModifiableTraversableSource#makeCollection()
-     */
-    public void makeCollection() throws SourceException {
-        // nothing to do
-    }
 
     // =========================================================================
     // ModifiableSource interface
@@ -75,103 +63,14 @@ public class FileSourceHelper extends AbstractJCRNodeSource implements
     /**
      * @see org.apache.excalibur.source.ModifiableSource#canCancel(java.io.OutputStream)
      */
-    public boolean canCancel(OutputStream stream) {
+    public static boolean canCancel(OutputStream stream) {
         return false;
     }
 
     /**
      * @see org.apache.excalibur.source.ModifiableSource#cancel(java.io.OutputStream)
      */
-    public void cancel(OutputStream stream) throws IOException {
+    public static void cancel(OutputStream stream) throws IOException {
     }
 
-    /**
-     * @see org.apache.excalibur.source.ModifiableSource#delete()
-     */
-    public void delete() throws SourceException {
-    }
-
-    /**
-     * @see org.apache.excalibur.source.ModifiableSource#getOutputStream()
-     */
-    public OutputStream getOutputStream() throws IOException {
-        return null;
-    }
-
-    // =========================================================================
-    // TraversableSource interface
-    // =========================================================================
-
-    /**
-     * @see org.apache.excalibur.source.TraversableSource#getChild(java.lang.String)
-     */
-    public Source getChild(String name) throws SourceException {
-        String path;
-        JCRSourceFactory factory = new JCRSourceFactory();
-        try {
-            Node child = node.getNode(name);
-            path = child.getPath();
-        } catch (Exception e) {
-            throw new SourceException("Child node not available.", e);
-        }
-        return factory.createSource(session, path);
-    }
-
-    /**
-     * @see org.apache.excalibur.source.TraversableSource#getChildren()
-     */
-    public Collection getChildren() throws SourceException {
-        List<Source> childs = new ArrayList<Source>();
-        try {
-            NodeIterator nit = node.getNodes();
-            while (nit.hasNext()) {
-                Node child = nit.nextNode();
-                childs.add(getChild(child.getName()));
-            }
-        } catch (RepositoryException e) {
-            throw new SourceException("A child node was not available.", e);
-        }
-        return childs;
-    }
-
-    /**
-     * @see org.apache.excalibur.source.TraversableSource#getName()
-     */
-    public String getName() {
-        try {
-            return node.getName();
-        } catch (RepositoryException e) {
-            return "";
-        }
-    }
-
-    /**
-     * @see org.apache.excalibur.source.TraversableSource#getParent()
-     */
-    public Source getParent() throws SourceException {
-        String path;
-        JCRSourceFactory factory = new JCRSourceFactory();
-        try {
-            Node child = node.getParent();
-            path = child.getPath();
-        } catch (Exception e) {
-            throw new SourceException("Parent node not available.", e);
-        }
-        return factory.createSource(session, path);
-    }
-
-    /**
-     * @see org.apache.excalibur.source.TraversableSource#isCollection()
-     */
-    public boolean isCollection() {
-        try {
-            if(node.isNodeType("nt:folder")) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (RepositoryException e) {
-            return false;
-        }
-    }
 }
--- mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/jcr/xml/source/helper/XMLFileSourceHelper.java
+++ mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/jcr/xml/source/helper/XMLFileSourceHelper.java
@@ -15,6 +15,10 @@ import javax.jcr.Property;
 import javax.jcr.PropertyIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.excalibur.source.ModifiableSource;
 import org.apache.excalibur.source.SourceException;
@@ -37,18 +41,8 @@ import com.mindquarry.jcr.xml.source.helper.stream.XMLFileOutputStream;
  * @author <a href="mailto:alexander(dot)saar(at)mindquarry(dot)com">Alexander
  *         Saar</a>
  */
-public class XMLFileSourceHelper extends AbstractJCRNodeSource implements
-        ModifiableSource, XMLizable {
-    /**
-     * Default contructor. Passes all parameters to the constructor of the super
-     * class.
-     * 
-     * {@link AbstractJCRNodeSource#AbstractJCRNodeSource(JCRSourceFactory, Session, String)}
-     */
-    public XMLFileSourceHelper(JCRSourceFactory factory, Session session,
-            String path) throws SourceException {
-        super(factory, session, path);
-    }
+public class XMLFileSourceHelper {
+
 
     // =========================================================================
     // XMLizable interface
@@ -59,7 +53,7 @@ public class XMLFileSourceHelper extends AbstractJCRNodeSource implements
      * 
      * @see org.apache.excalibur.xml.sax.XMLizable#toSAX(org.xml.sax.ContentHandler)
      */
-    public void toSAX(ContentHandler handler) throws SAXException {
+    public static void toSAX(ContentHandler handler, Node node) throws SAXException {
         JCRNodesToSAXConverter.convertToSAX(node, handler);
     }
 
@@ -72,19 +66,24 @@ public class XMLFileSourceHelper extends AbstractJCRNodeSource implements
      * 
      * @see org.apache.excalibur.source.Source#getInputStream()
      */
-    public InputStream getInputStream() throws IOException,
+    public static InputStream getInputStream(Node node) throws IOException,
             SourceNotFoundException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-        String xmlHead = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-        os.write(xmlHead.getBytes());
-
-        try {
-            getBytes(node.getNode("jcr:content"), os);
-        } catch (Exception e) {
-            throw new IOException("Error while reading repository content.");
-        }
-        return new ByteArrayInputStream(os.toByteArray());
+        SAXTransformerFactory stfactory = (SAXTransformerFactory) SAXTransformerFactory
+        .newInstance();
+        TransformerHandler handler;
+		try {
+			handler = stfactory.newTransformerHandler();
+			handler.setResult(new StreamResult(os));
+	        XMLFileSourceHelper.toSAX(handler, node);
+	        
+	        return new ByteArrayInputStream(os.toByteArray());
+		} catch (TransformerConfigurationException e) {
+			throw new IOException("Unable to configure Transformer Factory: " + e.getLocalizedMessage());
+		} catch (SAXException e) {
+			throw new IOException("Unable to serialize XML results: "  + e.getLocalizedMessage());
+		}
     }
 
     // =========================================================================
@@ -96,7 +95,7 @@ public class XMLFileSourceHelper extends AbstractJCRNodeSource implements
      * 
      * @see org.apache.excalibur.source.ModifiableSource#canCancel(java.io.OutputStream)
      */
-    public boolean canCancel(OutputStream stream) {
+    public static boolean canCancel(OutputStream stream) {
         if (stream instanceof XMLFileOutputStream) {
             return ((XMLFileOutputStream) stream).canCancel();
         } else {
@@ -109,82 +108,11 @@ public class XMLFileSourceHelper extends AbstractJCRNodeSource implements
      * 
      * @see org.apache.excalibur.source.ModifiableSource#cancel(java.io.OutputStream)
      */
-    public void cancel(OutputStream stream) throws IOException {
+    public static void cancel(OutputStream stream) throws IOException {
         if (canCancel(stream)) {
             ((XMLFileOutputStream) stream).cancel();
         } else {
             throw new IllegalArgumentException("Stream cannot be cancelled");
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.apache.excalibur.source.ModifiableSource#delete()
-     */
-    public void delete() throws SourceException {
-        try {
-            node.remove();
-            node = null;
-            session.save();
-        } catch (Exception e) {
-            throw new SourceException("Can not remove node due to version"
-                    + " or constraint problems.", e);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.apache.excalibur.source.ModifiableSource#getOutputStream()
-     */
-    public OutputStream getOutputStream() throws IOException {
-        return new XMLFileOutputStream(node, session);
-    }
-
-    // =========================================================================
-    // private methods
-    // =========================================================================
-
-    private void getBytes(Node node, ByteArrayOutputStream os)
-            throws IOException {
-        try {
-            NodeIterator nit = node.getNodes();
-            while (nit.hasNext()) {
-                Node child = nit.nextNode();
-                if (child.isNodeType("xt:text")) {
-                    InputStream is = child.getProperty("xt:characters")
-                            .getStream();
-
-                    int b;
-                    while ((b = is.read()) != -1) {
-                        os.write(b);
-                    }
-                } else if (child.isNodeType("xt:element")) {
-                    os.write("<".getBytes());
-                    os.write(child.getName().getBytes());
-
-                    PropertyIterator pit = child.getProperties();
-                    while (pit.hasNext()) {
-                        Property prop = pit.nextProperty();
-                        if (prop.getName().startsWith("jcr")) {
-                            continue;
-                        }
-                        String att = " " + prop.getName() + "=\""
-                                + prop.getString() + "\"";
-                        os.write(att.getBytes());
-                    }
-                    os.write(">".getBytes());
-
-                    getBytes(child, os);
-
-                    os.write("<".getBytes());
-                    os.write(child.getName().getBytes());
-                    os.write(">".getBytes());
-                }
-            }
-        } catch (RepositoryException e) {
-            throw new IOException("Error while reading repository content.");
         }
     }
 }
