e8d3998fb46ba2ea2547c4763ad7430dcb95ac65
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-09-26 09:51:43 +0000

    node-types.txt with necessary node types is now directly included and always loaded by the JackrabbitInitializer (configuration 'definitions' can now be used for additional node types)

--- mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/jcr/jackrabbit/JackrabbitInitializer.java
+++ mindquarry-jcr/trunk/mindquarry-jcr-source/src/main/java/com/mindquarry/jcr/jackrabbit/JackrabbitInitializer.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.jcr.NamespaceException;
 import javax.jcr.NamespaceRegistry;
 import javax.jcr.Node;
 import javax.jcr.Repository;
@@ -49,15 +50,24 @@ import org.apache.jackrabbit.core.nodetype.compact.ParseException;
  */
 public class JackrabbitInitializer implements Serviceable, Configurable,
         Initializable, Contextualizable {
+
+    public static final String MINDQUARRY_JCR_XML_NAMESPACE_PREFIX = "xt";
+
+    public static final String MINDQUARRY_JCR_XML_NAMESPACE_URI = "http://mindquarry.com/ns/cnd/xt";
+
+    public static final String MINDQUARRY_JCR_XML_NODETYPES_FILE = "resource://com/mindquarry/jcr/jackrabbit/node-types.txt";
+
     public static final String ROLE = JackrabbitInitializer.class.getName();
 
     private ServiceManager manager;
 
     private Configuration config;
-    
+
     private Context ctx;
 
     private Source defSource;
+
+    private Source additionalDefSource;
 
     private VariableResolver loginResolver;
 
@@ -68,7 +78,7 @@ public class JackrabbitInitializer implements Serviceable, Configurable,
      */
     public void configure(Configuration config) throws ConfigurationException {
         this.config = config;
-        
+
         Configuration credentials = config.getChild("credentials", false);
         if (credentials != null) {
             String login = credentials.getAttribute("login");
@@ -97,13 +107,14 @@ public class JackrabbitInitializer implements Serviceable, Configurable,
             }
         }
 
-        // get node definitions
+        // get node definitions (for additional definitions)
         Configuration defs = config.getChild("definitions");
         if (defs != null) {
             try {
                 SourceResolver resolver = (SourceResolver) manager
                         .lookup(SourceResolver.ROLE);
-                defSource = resolver.resolveURI(defs.getAttribute("src"));
+                additionalDefSource = resolver.resolveURI(defs
+                        .getAttribute("src"));
             } catch (Exception e) {
                 throw new ConfigurationException(
                         "Definitions file not found at "
@@ -113,6 +124,17 @@ public class JackrabbitInitializer implements Serviceable, Configurable,
             throw new ConfigurationException(
                     "Invalid expression for 'definitions' at "
                             + credentials.getLocation());
+        }
+
+        // get source for default node defintions
+        try {
+            SourceResolver resolver = (SourceResolver) this.manager
+                    .lookup(SourceResolver.ROLE);
+            defSource = resolver.resolveURI(MINDQUARRY_JCR_XML_NODETYPES_FILE);
+        } catch (Exception e) {
+            throw new ConfigurationException(
+                    "Cannot find internal configuration resource "
+                            + MINDQUARRY_JCR_XML_NODETYPES_FILE);
         }
     }
 
@@ -139,47 +161,43 @@ public class JackrabbitInitializer implements Serviceable, Configurable,
         // objectModel = ContextHelper.getObjectModel(ctx);
         // } catch (Exception e) {
         // throw new LoginException("No objectModel to evaluate credentials",
-        //                    e);
-        //        }
+        // e);
+        // }
         Configuration credentials = config.getChild("credentials", false);
         String login = credentials.getAttribute("login");
         String password = credentials.getAttribute("password");
-        
+
         // TODO us variable resolver for login & password retrieval
-        
+
         // try {
         // login = loginResolver.resolve(objectModel);
         // password = passwordResolver.resolve(objectModel);
         // } catch (PatternException e) {
         // throw new LoginException("Failed to evaluate credentials", e);
-        //        }
+        // }
 
         Repository repo = (Repository) manager.lookup(Repository.class
                 .getName());
         Session session = repo.login(new SimpleCredentials(login, password
                 .toCharArray()));
 
-        registerNodeTypes(session.getWorkspace(), defSource);
+        setupNamespacesAndNodeTypes(session.getWorkspace());
         setupInitialRepositoryStructure(session);
         session.save();
     }
 
-    private void registerNodeTypes(Workspace workspace, Source source)
+    private void setupNamespacesAndNodeTypes(Workspace workspace)
             throws ParseException, RepositoryException,
             InvalidNodeTypeDefException, SourceNotFoundException, IOException {
         // register xt:* namespace
         NamespaceRegistry nsRegistry = workspace.getNamespaceRegistry();
-        nsRegistry.registerNamespace("xt", "http://mindquarry.com/ns/cnd/xt");
-
-        InputStreamReader reader = new InputStreamReader(source
-                .getInputStream());
-
-        // Create a CompactNodeTypeDefReader
-        CompactNodeTypeDefReader cndReader = new CompactNodeTypeDefReader(
-                reader, source.getURI());
-
-        // Get the List of NodeTypeDef objects
-        List ntdList = cndReader.getNodeTypeDefs();
+        try {
+            // check if the namespace already exists
+            nsRegistry.getURI(MINDQUARRY_JCR_XML_NAMESPACE_PREFIX);
+        } catch (NamespaceException ne) {
+            nsRegistry.registerNamespace(MINDQUARRY_JCR_XML_NAMESPACE_PREFIX,
+                    MINDQUARRY_JCR_XML_NAMESPACE_URI);
+        }
 
         // Get the NodeTypeManager from the Workspace. Note that it must be
         // casted from the generic JCR NodeTypeManager to the
@@ -190,6 +208,33 @@ public class JackrabbitInitializer implements Serviceable, Configurable,
         // Acquire the NodeTypeRegistry
         NodeTypeRegistry ntreg = ntmgr.getNodeTypeRegistry();
 
+        // register the default node types (xt:element etc.)
+        registerNodeTypesFromTextFile(defSource, ntreg);
+
+        if (this.additionalDefSource != null) {
+            registerNodeTypesFromTextFile(this.additionalDefSource, ntreg);
+        }
+    }
+
+    private void registerNodeTypesFromTextFile(Source additionalSource,
+            NodeTypeRegistry ntreg) throws IOException,
+            SourceNotFoundException, ParseException,
+            InvalidNodeTypeDefException, RepositoryException {
+        InputStreamReader reader = new InputStreamReader(additionalSource
+                .getInputStream());
+
+        // Create a CompactNodeTypeDefReader
+        CompactNodeTypeDefReader cndReader = new CompactNodeTypeDefReader(
+                reader, additionalSource.getURI());
+
+        // Get the List of NodeTypeDef objects
+        List ntdList = cndReader.getNodeTypeDefs();
+
+        registerNodeTypes(ntdList, ntreg);
+    }
+
+    private void registerNodeTypes(List ntdList, NodeTypeRegistry ntreg)
+            throws InvalidNodeTypeDefException, RepositoryException {
         // Loop through the prepared NodeTypeDefs
         for (Iterator i = ntdList.iterator(); i.hasNext();) {
             // Get the NodeTypeDef...
