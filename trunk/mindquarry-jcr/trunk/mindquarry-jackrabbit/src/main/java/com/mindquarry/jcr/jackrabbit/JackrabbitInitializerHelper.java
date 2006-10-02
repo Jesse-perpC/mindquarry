3e9f8cd16b78dc9e2aa9df0fd9c8cc5ae378a272
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-10-02 11:11:07 +0000

    - added implementation for standalone jackrabbit repositories - fixed bug in repo initializer that occurs when the initialzer runs on an already initialzed repository

--- trunk/mindquarry-jcr/trunk/mindquarry-jackrabbit/src/main/java/com/mindquarry/jcr/jackrabbit/JackrabbitInitializerHelper.java
+++ trunk/mindquarry-jcr/trunk/mindquarry-jackrabbit/src/main/java/com/mindquarry/jcr/jackrabbit/JackrabbitInitializerHelper.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.jcr.NamespaceException;
 import javax.jcr.NamespaceRegistry;
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
@@ -29,72 +30,90 @@ import org.apache.jackrabbit.core.nodetype.compact.ParseException;
  *         Saar</a>
  */
 public class JackrabbitInitializerHelper {
-	public static final String MQ_JCR_XML_NAMESPACE_PREFIX = "xt";
+    public static final String MQ_JCR_XML_NAMESPACE_PREFIX = "xt";
 
-	public static final String MQ_JCR_XML_NAMESPACE_URI = "http://mindquarry.com/ns/cnd/xt";
+    public static final String MQ_JCR_XML_NAMESPACE_URI = "http://mindquarry.com/ns/cnd/xt";
 
-	public static void setupRepository(Session session,
-			InputStreamReader nDefs, String uri)
-			throws ParseException, RepositoryException,
-			InvalidNodeTypeDefException, SourceNotFoundException, IOException {
-		// register xt:* namespace
-		NamespaceRegistry nsRegistry = session.getWorkspace()
-				.getNamespaceRegistry();
-		try {
-			// check if the namespace already exists
-			nsRegistry.getURI(MQ_JCR_XML_NAMESPACE_PREFIX);
-		} catch (NamespaceException ne) {
-			nsRegistry.registerNamespace(MQ_JCR_XML_NAMESPACE_PREFIX,
-					MQ_JCR_XML_NAMESPACE_URI);
-		}
+    public static void setupRepository(Session session,
+            InputStreamReader nDefs, String uri) throws ParseException,
+            RepositoryException, InvalidNodeTypeDefException,
+            SourceNotFoundException, IOException {
+        // register xt:* namespace
+        NamespaceRegistry nsRegistry = session.getWorkspace()
+                .getNamespaceRegistry();
+        try {
+            // check if the namespace already exists
+            nsRegistry.getURI(MQ_JCR_XML_NAMESPACE_PREFIX);
+        } catch (NamespaceException ne) {
+            nsRegistry.registerNamespace(MQ_JCR_XML_NAMESPACE_PREFIX,
+                    MQ_JCR_XML_NAMESPACE_URI);
+        }
 
-		// Get the NodeTypeManager from the Workspace. Note that it must be
-		// casted from the generic JCR NodeTypeManager to the
-		// Jackrabbit-specific implementation.
-		NodeTypeManagerImpl ntmgr = (NodeTypeManagerImpl) session
-				.getWorkspace().getNodeTypeManager();
+        // Get the NodeTypeManager from the Workspace. Note that it must be
+        // casted from the generic JCR NodeTypeManager to the
+        // Jackrabbit-specific implementation.
+        NodeTypeManagerImpl ntmgr = (NodeTypeManagerImpl) session
+                .getWorkspace().getNodeTypeManager();
 
-		// Acquire the NodeTypeRegistry
-		NodeTypeRegistry ntreg = ntmgr.getNodeTypeRegistry();
+        // Acquire the NodeTypeRegistry
+        NodeTypeRegistry ntreg = ntmgr.getNodeTypeRegistry();
 
-		// register the default node types (xt:element etc.)
-		registerNodeTypesFromTextFile(nDefs, ntreg, uri);
+        // register the default node types (xt:element etc.)
+        registerNodeTypesFromTextFile(nDefs, ntreg, uri);
 
-		setupInitialRepositoryStructure(session);
-	}
+        setupInitialRepositoryStructure(session);
+    }
 
-	private static void registerNodeTypesFromTextFile(InputStreamReader reader,
-			NodeTypeRegistry ntreg, String uri) throws IOException,
-			SourceNotFoundException, ParseException,
-			InvalidNodeTypeDefException, RepositoryException {
-		// Create a CompactNodeTypeDefReader
-		CompactNodeTypeDefReader cndReader = new CompactNodeTypeDefReader(
-				reader, uri);
+    private static void registerNodeTypesFromTextFile(InputStreamReader reader,
+            NodeTypeRegistry ntreg, String uri) throws IOException,
+            SourceNotFoundException, ParseException,
+            InvalidNodeTypeDefException, RepositoryException {
+        // Create a CompactNodeTypeDefReader
+        CompactNodeTypeDefReader cndReader = new CompactNodeTypeDefReader(
+                reader, uri);
 
-		// Get the List of NodeTypeDef objects
-		List ntdList = cndReader.getNodeTypeDefs();
+        // Get the List of NodeTypeDef objects
+        List ntdList = cndReader.getNodeTypeDefs();
 
-		registerNodeTypes(ntdList, ntreg);
-	}
+        registerNodeTypes(ntdList, ntreg);
+    }
 
-	private static void registerNodeTypes(List ntdList, NodeTypeRegistry ntreg)
-			throws InvalidNodeTypeDefException, RepositoryException {
-		// Loop through the prepared NodeTypeDefs
-		for (Iterator i = ntdList.iterator(); i.hasNext();) {
-			// Get the NodeTypeDef...
-			NodeTypeDef ntd = (NodeTypeDef) i.next();
+    private static void registerNodeTypes(List ntdList, NodeTypeRegistry ntreg)
+            throws RepositoryException {
+        // Loop through the prepared NodeTypeDefs
+        for (Iterator i = ntdList.iterator(); i.hasNext();) {
+            // Get the NodeTypeDef...
+            NodeTypeDef ntd = (NodeTypeDef) i.next();
 
-			// ...and register it
-			ntreg.registerNodeType(ntd);
-		}
-	}
+            // ...and register it
+            try {
+                ntreg.registerNodeType(ntd);
+            } catch (InvalidNodeTypeDefException e) {
+                // seems that the node type was already registered, so just
+                // print a message and continue.
+                System.out.println(e.getLocalizedMessage());
+            }
+        }
+    }
 
-	private static void setupInitialRepositoryStructure(Session session)
-			throws RepositoryException {
-		// add users folder
-		Node root = session.getRootNode();
-		root.addNode("users", "nt:folder");
-		root.addNode("teamspaces", "nt:folder");
-		root.addNode("tags", "nt:folder");
-	}
+    private static void setupInitialRepositoryStructure(Session session)
+            throws RepositoryException {
+        // add necessary folders folder
+        Node root = session.getRootNode();
+        try {
+            root.getNode("users");
+        } catch (PathNotFoundException e) {
+            root.addNode("users", "nt:folder");
+        }
+        try {
+            root.getNode("teamspaces");
+        } catch (PathNotFoundException e) {
+            root.addNode("teamspaces", "nt:folder");
+        }
+        try {
+            root.getNode("tags");
+        } catch (PathNotFoundException e) {
+            root.addNode("tags", "nt:folder");
+        }
+    }
 }
