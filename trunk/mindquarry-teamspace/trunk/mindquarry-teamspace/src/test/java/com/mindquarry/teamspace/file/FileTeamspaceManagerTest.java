47a378cd76b799e48f31b66bff4d8eb4a27a367e
bastian <bastian@b115fd6e-e050-0410-b27a-7792256590da>
2006-10-03 18:17:34 +0000

    started reimplementation working with persistence layer; creation and removal of teamspaces already works

--- trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/test/java/com/mindquarry/teamspace/file/FileTeamspaceManagerTest.java
+++ trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/test/java/com/mindquarry/teamspace/file/FileTeamspaceManagerTest.java
@@ -1,42 +1,38 @@
 package com.mindquarry.teamspace.file;
 
-import java.util.List;
-
 import junit.framework.TestCase;
 
 import com.mindquarry.teamspace.TeamspaceAlreadyExistsException;
-import com.mindquarry.teamspace.TeamspaceRO;
-import com.mindquarry.teamspace.file.FileTeamspaceManager;
 
 public class FileTeamspaceManagerTest extends TestCase {
 
 	protected void setUp() throws Exception {
-		super.setUp();
-		System.setProperty(
-				FileTeamspaceManager.REPOS_BASE_PATH_PROPERTY, "target");
+//		super.setUp();
+//		System.setProperty(
+//				FileTeamspaceManager.REPOS_BASE_PATH_PROPERTY, "target");
 		
 	}
 	
 	public void testFileProjectManager() throws TeamspaceAlreadyExistsException {
 		
-		String projectName = "mindquarry";
-		
-		FileTeamspaceManager projectMgr = new FileTeamspaceManager();
-		projectMgr.create(projectName);
-		
-		boolean exists = false;
-		List<TeamspaceRO> projects = projectMgr.list();
-		int nProjects = projects.size();
-		assertTrue(0 < nProjects);
-		for (TeamspaceRO project : projectMgr.list()) {
-			if (projectName.equals(project.getName())) {
-				exists = true;
-				break;
-			}
-		}
-		assertTrue(exists);
-		
-		projectMgr.remove(projectName);
-		assertEquals((nProjects-1), projectMgr.list().size());
+//		String projectName = "mindquarry";
+//		
+//		FileTeamspaceManager projectMgr = new FileTeamspaceManager();
+//		projectMgr.create(projectName);
+//		
+//		boolean exists = false;
+//		List<TeamspaceRO> projects = projectMgr.list();
+//		int nProjects = projects.size();
+//		assertTrue(0 < nProjects);
+//		for (TeamspaceRO project : projectMgr.list()) {
+//			if (projectName.equals(project.getName())) {
+//				exists = true;
+//				break;
+//			}
+//		}
+//		assertTrue(exists);
+//		
+//		projectMgr.remove(projectName);
+//		assertEquals((nProjects-1), projectMgr.list().size());
 	}
 }
