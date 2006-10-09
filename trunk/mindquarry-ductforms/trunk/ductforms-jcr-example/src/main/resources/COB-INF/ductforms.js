945d33f0f9f6709bcff367ffa9d695153c56cfbf
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-10-09 09:17:16 +0000

    added status and progress report pages

--- trunk/mindquarry-ductforms/trunk/ductforms-jcr-example/src/main/resources/COB-INF/ductforms.js
+++ trunk/mindquarry-ductforms/trunk/ductforms-jcr-example/src/main/resources/COB-INF/ductforms.js
@@ -4,35 +4,49 @@ var myform;
 var mypage;
 var pipeline = false;
 
-function edit(form, page) {
+// helper function
+function loadAndShow(form, page) {
 	form.loadXML("cocoon:/" + page + ".xml.plain");
 	form.showForm(page+".xml.instance");
-	//the form includes all possible fields, but only some of them are
-	//actually used. the best solution would be to strip out forms
+}
+
+// this triggers the edit variant of the form (called from sitemap)
+function editPage(form) {
+	var page = cocoon.parameters["page"];
+	
+	// save form and page for AJAX calls
+	mypage = page;
+	myform = form;
+
+	loadAndShow(form, page);
+	
+	// the form includes all possible fields, but only some of them are
+	// actually used. the best solution would be to strip out forms
 	form.saveXML("jcr:///test/" + page + ".xml","xslt/ductforms/saveclean.xsl");
-	//form.saveXML("xmldb:xindice-embed:///db/test/" + page + ".xml");
+	
+	// reset form and page for AJAX calls
 	myform = null;
 	mypage = null;
+	
+	// show the display variant of the form
 	cocoon.redirectTo(page+".xml", false);
 }
 
-function editPage(form) {
-	mypage = cocoon.parameters["page"];
-	myform = form;
-	print("editing page " + mypage + " from cocoon");
-	edit(myform, mypage);
-}
-
+// this simply displays the form in readonly mode (called from sitemap)
 function showPage(form) {
+	var page = cocoon.parameters["page"];
+	
+	// no AJAX calls are allowed in display mode
 	myform = null;
 	mypage = null;
-	var page = cocoon.parameters["page"];
-	//set status to output only.
+	
+	// set status to output only
 	form.lookupWidget("/").setState(Packages.org.apache.cocoon.forms.formmodel.WidgetState.OUTPUT);
-	form.loadXML("cocoon:/" + page + ".xml.plain");
-	form.showForm(page+".xml.instance");
+	
+	loadAndShow(form, page);
 }
 
+// called by AJAX for mutivalue fields
 function upd(event) {
 	if (myform&&mypage) {
 		myform.saveXML("jcr:///test/" + mypage + ".xml","xslt/ductforms/saveclean.xsl");
@@ -40,9 +54,9 @@ function upd(event) {
 	cocoon.exit();
 }
 
-//ok, I admit Javascript seriously rocks. I am overloading this function
-//with another parameter to support transformations of the xml document
-//before actually saving
+// ok, I admit Javascript seriously rocks. I am overloading this function
+// with another parameter to support transformations of the xml document
+// before actually saving
 Form.prototype.saveXML = function(uri, xsluri) {
     var source = null;
     var resolver = null;
