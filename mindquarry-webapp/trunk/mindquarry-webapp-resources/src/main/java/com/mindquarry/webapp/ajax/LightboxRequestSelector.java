6a43c6b07c82f419dd98ce0402658aff7988cb91
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-10-16 08:20:08 +0000

    new request selector for ajax-lightbox

---
+++ mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/java/com/mindquarry/webapp/ajax/LightboxRequestSelector.java
@@ -0,0 +1,25 @@
+package com.mindquarry.webapp.ajax;
+
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.selection.AbstractSwitchSelector;
+import org.apache.commons.lang.BooleanUtils;
+
+public class LightboxRequestSelector extends AbstractSwitchSelector {
+    public static final String LIGHTBOX_REQUEST = "lightbox-request"; //$NON-NLS-1$
+    
+    public Object getSelectorContext(Map objectModel, Parameters parameters) {
+        Request req = ObjectModelHelper.getRequest(objectModel);
+        
+        return BooleanUtils.toBooleanObject(req.getParameter(LIGHTBOX_REQUEST)!=null);
+    }
+
+    public boolean select(String expression, Object selectorContext) {
+        boolean test = BooleanUtils.toBoolean(expression);
+        return test == ((Boolean)selectorContext).booleanValue();
+    }
+
+}
