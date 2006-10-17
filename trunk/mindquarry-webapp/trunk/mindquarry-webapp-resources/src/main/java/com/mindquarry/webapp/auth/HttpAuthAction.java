fee81a9ab40c2490148f1ccfd306df47f2e212fa
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-10-17 08:54:18 +0000

    better auth

--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/java/com/mindquarry/webapp/auth/HttpAuthAction.java
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/java/com/mindquarry/webapp/auth/HttpAuthAction.java
@@ -1,5 +1,6 @@
 package com.mindquarry.webapp.auth;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.avalon.framework.parameters.Parameters;
@@ -19,26 +20,31 @@ public class HttpAuthAction extends AbstractAction {
 			Parameters params) throws Exception {
 		Request request = ObjectModelHelper.getRequest(objectModel);
 		Response response = ObjectModelHelper.getResponse(objectModel);
+		String realm = params.getParameter("realm", "Protected Resource");
+
+		//always send an authentication request
+		response.setHeader("WWW-Authenticate", "BASIC realm=\""+ realm +"\"");
 		
 		String auth = request.getHeader("Authorization");
-		if (auth.toUpperCase().startsWith("BASIC")) {
-			String user = getAuthenticatedUser(auth.substring(6));
-			if (user==null) {
-				//not authenticated. the browser sends an authentication request
-				response.setHeader("WWW-Authenticate", "BASIC realm=\""+ params.getParameter("realm") +"\"");
-				if (response instanceof HttpResponse) {
-					HttpResponse httpresponse = (HttpResponse) response;
-					httpresponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-				}
-			} else {
-				//authenticated. the username is now available from the sitemap
-				Map responsemap = EMPTY_MAP;
-				responsemap.put("username", user);
-				return responsemap;
-			}
+		String user = null;
+		if ((auth!=null)&&(auth.toUpperCase().startsWith("BASIC"))) {
+			user = getAuthenticatedUser(auth.substring(6));
+			
 		}
 		
-		return null;
+		if (user==null) {
+			//not authenticated. the browser sends an authentication request
+			if (response instanceof HttpResponse) {
+				HttpResponse httpresponse = (HttpResponse) response;
+				httpresponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+			}
+			return EMPTY_MAP;
+		} else {
+			//authenticated. the username is now available from the sitemap
+			Map responsemap = new HashMap();
+			responsemap.put("username", user);
+			return responsemap;
+		}
 	}
 	
 	private String getAuthenticatedUser(String encodedAuth) {
@@ -49,6 +55,7 @@ public class HttpAuthAction extends AbstractAction {
 			String password = auths[1];
 			//TODO: lookup the user manager and verify the password
 			
+			//return the username if authentication was successful
 			return username;
 		}
 		return null;
