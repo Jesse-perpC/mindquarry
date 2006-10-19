42a076d918470e5faa6fe6d29039bb05dd3dc51c
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-10-19 08:50:01 +0000

    added sitemap action for logging stuff

---
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/java/com/mindquarry/webapp/acting/DebugLogAction.java
@@ -0,0 +1,34 @@
+/*
+ * Coypright (c) 2006 Mindquarry GmbH, Potsdam, Germany 
+ */
+package com.mindquarry.webapp.acting;
+
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.thread.ThreadSafe;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.SourceResolver;
+
+/**
+ * @author <a href="mailto:alexander(dot)klimetschek(at)mindquarry(dot)com">
+ *         Alexander Klimetschek</a>
+ * 
+ */
+public class DebugLogAction extends AbstractAction implements ThreadSafe {
+
+    /**
+     * An action that logs the string in the src attribute.
+     * 
+     */
+    public Map act(Redirector redirector, SourceResolver resolver,
+            Map objectModel, String src, Parameters par) throws Exception {
+
+        System.err.println(src);
+        getLogger().info(src);
+
+        return null;
+    }
+
+}
