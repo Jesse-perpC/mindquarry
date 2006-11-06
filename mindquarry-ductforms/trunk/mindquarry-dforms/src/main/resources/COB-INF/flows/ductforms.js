be2197eeb4b8d65ba62b8be47401ebd1a2acbe43
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-06 12:25:04 +0000

    removed logs

--- mindquarry-ductforms/trunk/mindquarry-dforms/src/main/resources/COB-INF/flows/ductforms.js
+++ mindquarry-ductforms/trunk/mindquarry-dforms/src/main/resources/COB-INF/flows/ductforms.js
@@ -6,64 +6,51 @@ var documentPath_;
 
 // helper function
 function loadAndShow(form, filename) {
-	print("############ loadAndShow()");
-	print("############   loading xml: cocoon:/" + filename + ".plain");
 	form.loadXML("cocoon:/" + filename + ".plain");
-	print("############   showing form: " + filename + ".instance");
 	form.showForm(filename + ".instance");
 }
 
 // this triggers the edit variant of the form (called from sitemap)
 function editPage(form) {
-	print("############ editPage()");
 	var baseURI = cocoon.parameters["baseURI"];
 	var documentID = cocoon.parameters["documentID"];
 	var filename = documentID + ".xml";
 	
-	print("############   got documentID and filename");
 	// save form and page for auto-reload or AJAX calls
 	form_ = form;
 	documentPath_ = baseURI + filename;
 
 	loadAndShow(form, filename);
 	
-	print("############   saving file");
 	// the form includes all possible fields, but only some of them are
 	// actually used. the best solution would be to strip out forms
 	form.saveXML(baseURI + filename, CLEAN_MODEL_XSL);
 	
-	print("############   resetting vars");
 	// reset form and page for auto-reload or AJAX calls
 	form_ = null;
 	documentPath_ = null;
 	
-	print("############   redirecting to show page");
 	// show the display variant of the form
 	cocoon.redirectTo("cocoon:/show-document?baseURI=" + baseURI + "&documentID=" + documentID, false);
 }
 
 // this simply displays the form in readonly mode (called from sitemap)
 function showPage(form) {
-	print("############ showPage()");
 	var documentID = cocoon.parameters["documentID"];
 	var filename = documentID + ".xml";
 	
-	print("############   got documentID and filename");
 	// no auto-reload or AJAX calls are allowed in display mode
 	form_ = null;
 	documentPath_ = null;
 	
-	print("############   now setting widget state");
 	// set status to output only
 	form.lookupWidget("/").setState(Packages.org.apache.cocoon.forms.formmodel.WidgetState.OUTPUT);
 	
-	print("############   calling loadAndShowPage()");
 	loadAndShow(form, filename);
 }
 
 // called by auto-reload or AJAX for mutivalue fields
 function upd(event) {
-	print("############ upd()");
 	if (form_ && documentPath_) {
 		form_.saveXML(documentPath_, CLEAN_MODEL_XSL);
 	}
@@ -74,7 +61,6 @@ function upd(event) {
 // with another parameter to support transformations of the xml document
 // before actually saving
 Form.prototype.saveXML = function(uri, xsluri) {
-	print("############ saveXML()");
     var source = null;
     var resolver = null;
     var outputStream = null;
@@ -83,7 +69,6 @@ Form.prototype.saveXML = function(uri, xsluri) {
         resolver = cocoon.getComponent(Packages.org.apache.cocoon.environment.SourceResolver.ROLE);
         source = resolver.resolveURI(uri);
         xslsource = new Packages.javax.xml.transform.stream.StreamSource(resolver.resolveURI(xsluri).getInputStream());
-		//print(xslsource);
 
         var tf = Packages.javax.xml.transform.TransformerFactory.newInstance();
 
@@ -98,7 +83,6 @@ Form.prototype.saveXML = function(uri, xsluri) {
             transformerHandler.setResult(new Packages.javax.xml.transform.stream.StreamResult(outputStream));
             this.getXML().toSAX(transformerHandler);
         } else {
-			print("############   excpetion");
             throw new Packages.org.apache.cocoon.ProcessingException("Cannot write to source " + uri);
         }
 
