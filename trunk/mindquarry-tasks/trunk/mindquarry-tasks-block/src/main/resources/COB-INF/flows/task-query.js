778b7e7244dfbce18f0839a802c827aece0f111e
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-10 14:21:30 +0000

    - added query generation and execution

--- trunk/mindquarry-tasks/trunk/mindquarry-tasks-block/src/main/resources/COB-INF/flows/task-query.js
+++ trunk/mindquarry-tasks/trunk/mindquarry-tasks-block/src/main/resources/COB-INF/flows/task-query.js
@@ -1,29 +1,6 @@
 /*
  * Copyright (C) 2006 Mindquarry GmbH, All Rights Reserved
  */
-var source;
-var srcResolver;
-var is;
-
-try {
-	srcResolver = cocoon.getComponent(Packages.org.apache.cocoon.environment.SourceResolver.ROLE);
-	source = srcResolver.resolveURI("/teamspaces/mindquarry/metadata.xml");
-	is = source.getInputStream();
-} finally {
-	if (source != null) {
-		srcResolver.release(source);
-	}
-	cocoon.releaseComponent(srcResolver);
-	
-	if (is != null) {
-		try {
-			is.close();
-		} catch (error) {
-			cocoon.log.error("Could not flush/close outputstream: " + error);
-		}
-	}
-}
-
 cocoon.load("resource://org/apache/cocoon/forms/flow/javascript/Form.js");
 var form_;
 
@@ -33,27 +10,70 @@ function displayQueryForm() {
 }
 
 function executeQuery(event) {
+	// validate query widget
+	form_.getWidget().validate();
+	if(!form_.getWidget().isValid()) return;
+	
+	// retrieve model
 	var model = form_.getModel();
-    
     var repeater = form_.lookupWidget("/parts");
-    var output = form_.lookupWidget("/xpath");
-    var xpath = "//task[";
     
+	// build query
+    var query = "jcr:///teamspaces/*/tasks?" + "/*";
+
     var rowCount = repeater.getSize();
-    for(var i = 0; i < rowCount; i++) {
-    	// check if this is the first query item, if not prepend an 'and'
-    	if(i > 0) {
-    		xpath = xpath + " and "
-    	}
+    if (rowCount > 0) {
+    	query = query + "[";
     	
-    	// evaluate query item
-    	var fieldWidget = repeater.getWidget(i, "field");
-    	var valueWidget = repeater.getWidget(i, "value");
-    	if(fieldWidget.getValue() != null) {
-    		xpath = xpath + "//" + fieldWidget.getValue().toLowerCase() + "='" +
-    			valueWidget.getValue() + "'";
-    	}
-    }
-    xpath = xpath + "]";
-    output.setValue(xpath);
+	    for(var i = 0; i < rowCount; i++) {
+	    	// check if this is the first query item, if not prepend an 'and'
+	    	if(i > 0) {
+	    		query = query + " and "
+	    	}
+	    	
+	    	// evaluate query item
+	    	var fieldWidget = repeater.getWidget(i, "field");
+	    	var selectorWidget = repeater.getWidget(i, "selector");
+	    	var valueWidget = repeater.getWidget(i, "value");
+	    		    	
+			if (selectorWidget.getValue() == "equals") {
+				query = query + ".//" + fieldWidget.getValue().toLowerCase()
+					 + "='" + valueWidget.getValue() + "'";
+			} else if (selectorWidget.getValue() == "contains") {
+				query = query + "contains(.//" + 
+					fieldWidget.getValue().toLowerCase() + ",'" + 
+					valueWidget.getValue() + "')";
+			}
+	    }
+	    query = query + "]";
+	}
+	// execute query
+	var srcResolver;
+	try {
+		srcResolver = cocoon.getComponent(Packages.org.apache.cocoon.environment.SourceResolver.ROLE);
+		source = srcResolver.resolveURI(query);
+		
+		// process result
+		var results = source.getChildren();
+		for(var i = 0; i < results.size(); i++) {
+			var source = results.get(i);
+			
+			var bf = Packages.javax.xml.parsers.DocumentBuilderFactory.newInstance();
+		    var builder = bf.newDocumentBuilder();
+		    var doc = builder.parse(source.getInputStream());
+		    
+		    var format = new Packages.org.apache.xml.serialize.OutputFormat();
+		    format.setLineWidth(65);
+		    format.setIndenting(true);
+		    format.setIndent(2);
+		    
+		    var serializer = new Packages.org.apache.xml.serialize.XMLSerializer(Packages.java.lang.System.out, format);
+		    serializer.serialize(doc);
+		}
+	} finally {
+		if (source != null) {
+			srcResolver.release(source);
+		}
+		cocoon.releaseComponent(srcResolver);
+	}
 }
