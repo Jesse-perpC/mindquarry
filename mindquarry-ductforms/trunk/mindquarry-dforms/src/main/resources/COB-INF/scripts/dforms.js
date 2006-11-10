4a89fb98f14a07b4a9677a9f9dad319bff7fb680
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-10 19:13:10 +0000

    switched to ajaxified forms

---
+++ mindquarry-ductforms/trunk/mindquarry-dforms/src/main/resources/COB-INF/scripts/dforms.js
@@ -0,0 +1,12 @@
+dojo.require("cocoon.forms.*");
+
+var result = new PopupWindow();
+result.forms_onload = function() {
+    //alert("hello");
+}
+
+forms_onloadHandlers.push(result);
+    
+var sayHelloFunction = function sayHello() {
+	alert("say hello");
+}
