e5038c24b3dca6edd2a40b7ee71b309659c6a477
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-11 22:29:28 +0000

    autoedit widget works

--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/AutoActiveField.js
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/AutoActiveField.js
@@ -7,8 +7,12 @@ dojo.require("dojo.event");
 dojo.widget.tags.addParseTreeHandler("dojo:AutoActiveField");
 dojo.widget.manager.registerWidgetPackage("mindquarry.widget");
 
+var submitagain = true;
+
 mindquarry.widget.AutoActiveField = function() {
 	dojo.widget.DomWidget.call(this);
+	
+	var cform = null;
 }
 
 dojo.inherits(mindquarry.widget.AutoActiveField, dojo.widget.DomWidget);
@@ -18,17 +22,22 @@ dojo.lang.extend(mindquarry.widget.AutoActiveField, {
 	isContainer: true,
 	
 	    buildRendering: function(args, parserFragment, parentWidget) {
-	    alert("building rendering");
         // Magical statement to get the dom node, stolen in DomWidget
 	    this.domNode = parserFragment["dojo:"+this.widgetType.toLowerCase()].nodeRef;
-	    this.domNode.innerHTML  ="hallo";
+	    this.cform = parentWidget;
 	    
 	    dojo.event.connect(this.domNode, "onclick", this, "onClick");
     },
     
     onClick: function(event) {
         event.preventDefault();
-        alert("clicked");
+        if (submitagain) {
+        	submitagain = false;
+        	this.cform.submit("ductform.ductforms_switch" ,{activate : this.domNode.id});
+        	// If real submit has to occur, it's taken care of in submit()
+        	setTimeout(function() {submitagain = true}, 1000);
+        }
+        return false;
     }
 	
 });
