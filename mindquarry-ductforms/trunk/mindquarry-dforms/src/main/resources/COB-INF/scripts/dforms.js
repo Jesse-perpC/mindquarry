03f7e6f55de81be925a3e6202be5a41facd3cf92
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-11 22:46:04 +0000

    enable title field onclikc

--- mindquarry-ductforms/trunk/mindquarry-dforms/src/main/resources/COB-INF/scripts/dforms.js
+++ mindquarry-ductforms/trunk/mindquarry-dforms/src/main/resources/COB-INF/scripts/dforms.js
@@ -1,14 +1,5 @@
 dojo.require("cocoon.forms.*");
 
-//var djConfig = { isDebug: true };
-
-dojo.event.connect (window, "onload" , function() {
-            sayHello();
-        });
-
-function sayHello() {
-	alert("helloing from dforms");
-}
 /*
 var result = new PopupWindow();
 result.forms_onload = function() {
