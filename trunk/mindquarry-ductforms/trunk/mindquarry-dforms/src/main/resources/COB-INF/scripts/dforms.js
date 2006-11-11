c0a7a21a3ac2688132a90b7ce9bd83d07ff674e5
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-11 21:58:54 +0000

    dodjo

--- trunk/mindquarry-ductforms/trunk/mindquarry-dforms/src/main/resources/COB-INF/scripts/dforms.js
+++ trunk/mindquarry-ductforms/trunk/mindquarry-dforms/src/main/resources/COB-INF/scripts/dforms.js
@@ -1,5 +1,15 @@
 dojo.require("cocoon.forms.*");
 
+//var djConfig = { isDebug: true };
+
+dojo.event.connect (window, "onload" , function() {
+            sayHello();
+        });
+
+function sayHello() {
+	alert("helloing from dforms");
+}
+/*
 var result = new PopupWindow();
 result.forms_onload = function() {
     //alert("hello");
@@ -10,3 +20,4 @@ forms_onloadHandlers.push(result);
 var sayHelloFunction = function sayHello() {
 	alert("say hello");
 }
+*/
