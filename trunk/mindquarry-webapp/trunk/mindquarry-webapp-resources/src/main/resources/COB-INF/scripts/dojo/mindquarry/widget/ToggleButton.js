168191dad8cfdc3c36a65f02a91d3ccd6812f360
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-12 20:50:05 +0000

    better styling of boxes

--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/ToggleButton.js
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/ToggleButton.js
@@ -2,12 +2,14 @@ dojo.provide("mindquarry.widget.ToggleButton");
 
 dojo.require("dojo.widget.*");
 dojo.require("dojo.html");
+dojo.require("dojo.dom");
 dojo.require("dojo.event");
 
 dojo.widget.tags.addParseTreeHandler("dojo:ToggleButton");
 dojo.widget.manager.registerWidgetPackage("mindquarry.widget");
 mindquarry.widget.ToggleButton = function() {
 	dojo.widget.DomWidget.call(this);
+	var checkbox = null;
 }
 
 dojo.inherits(mindquarry.widget.ToggleButton, dojo.widget.DomWidget);
@@ -19,13 +21,29 @@ dojo.lang.extend(mindquarry.widget.ToggleButton, {
 	    buildRendering: function(args, parserFragment, parentWidget) {
         // Magical statement to get the dom node, stolen in DomWidget
 	    this.domNode = parserFragment["dojo:"+this.widgetType.toLowerCase()].nodeRef;
-	    //dojo.event.connect(this.domNode, "onclick", this, "onClick");
+	    this.checkbox = dojo.dom.getFirstChildElement(this.domNode, "input");
+	    
+	    dojo.event.connect(this.domNode, "onclick", this, "onClick");
+	    //dojo.event.connect(this.checkbox, "onvaluechange", this, "adjustClasses");
     },
     
     onClick: function(event) {
         event.preventDefault();
-        //alert("clicked");
-        return false;
+        this.checkbox.checked = !this.checkbox.checked;
+        this.adjustClasses(event);
+    },
+    
+    adjustClasses: function(event) {
+        event.preventDefault();
+        if (this.checkbox.checked) {
+        	//alert("checked");
+        	dojo.html.replaceClass(this.domNode, "togglebuttonpushed", "togglebutton");
+        	//this.domNode.classname="togglebuttonpushed";
+        } else {
+        	//alert("unchecked");
+        	dojo.html.replaceClass(this.domNode, "togglebutton", "togglebuttonpushed");
+        	//this.domNode.classname="togglebutton";
+        }
     }
 	
 });
