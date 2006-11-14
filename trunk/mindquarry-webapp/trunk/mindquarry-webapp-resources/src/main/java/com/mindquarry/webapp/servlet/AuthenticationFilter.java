3b5850fe5b195e6a1e348124be483b908afe5484
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-14 16:20:49 +0000

    documented AuthenticationFilter class beautified login/logout styling cleaned up code

--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/java/com/mindquarry/webapp/servlet/AuthenticationFilter.java
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/java/com/mindquarry/webapp/servlet/AuthenticationFilter.java
@@ -24,20 +24,93 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import com.mindquarry.teamspace.Authentication;
 
 /**
- * Add summary documentation here.
- *
- * @author 
- * <a href="mailto:bastian.steinert(at)mindquarry.com">your full name</a>
+ * Servlet filter that handles HTTP authentication.
+ * 
+ * <p>
+ * The basic principle ist a RESTful authentication, ie. fully stateless. The
+ * second feature is to avoid the ugly standard HTTP login form in most popular
+ * GUI browsers by having a special login page that uses Javascript and
+ * XMLHttpRequests to do the login in the background. Because Javascript may not
+ * be available (because users are paranoid, use something like lynx or it is
+ * actually a REST client application), the process of using the standard way is
+ * also supported.
+ * </p>
+ * 
+ * <p>
+ * This filter needs appropriate client-side Javascript and login pages to work
+ * properly. The most important requirement is to provide a login page under
+ * LOGIN_PAGE that includes either a link to
+ * LOGIN_REQUEST_URI?LOGIN_REQUEST_PARAM=LOGIN_REQUEST_VALUE&TARGET_URI_PARAM=foobar
+ * (where foobar is passed to the login page as parameter with the same name,
+ * TARGET_URI_PARAM) or some Javascript code that uses XMLHttpRequest to first
+ * call LOGIN_REQUEST_URI?LOGIN_REQUEST_PARAM=LOGIN_REQUEST_VALUE and then
+ * (on success) redirecting the browser to the value of TARGET_URI_PARAM (eg.
+ * foobar).
+ * </p>
+ * 
+ * @author <a href="mailto:bastian(dot)steinert(at)mindquarry(dot)com">
+ *         Bastian Steinert</a>
+ * @author <a href="mailto:alexander(dot)klimetschek(at)mindquarry(dot)com">
+ *         Alexander Klimetschek</a>
  */
 public class AuthenticationFilter implements Filter {
 
+    /**
+     * The name of the request attribute that will hold the name of the
+     * authenticated user when the authentication was successful. Can then be
+     * used in Servlets and Cocoon Sitemaps via "{request-attr:username}".
+     */
+    public static final String USERNAME_ATTR = "username";
+
+    /**
+     * URI against which the actual first (hopefully successful) authentication
+     * is done, with ?LOGIN_REQUEST_PARAM=LOGIN_REQUEST_VALUE. Must be used in
+     * the login page.
+     */
+    public static final String LOGIN_REQUEST_URI = "/";
+
+    /**
+     * Name of the request parameter that is used to indicate an actual first
+     * login request. The only value used is LOGIN_REQUEST_VALUE.
+     */
+    public static final String LOGIN_REQUEST_PARAM = "request";
+
+    /**
+     * Standard value for LOGIN_REQUEST_PARAM that indicates a login request.
+     */
+    public static final String LOGIN_REQUEST_VALUE = "login";
+
+    /**
+     * URI of the login page to which any unauthorized GUI HTML browsers are
+     * sent to. Must display a link to the LOGIN_REQUEST_URI (including the
+     * param) or a form that uses Javascript XMLHttpRequest to call that URI.
+     * This is an unprotected page.
+     */
+    public static final String LOGIN_PAGE = "/loginpage";
+
+    /**
+     * URI of the logout page that should simply display that the logout was
+     * successful (and my be a link to re-login). This is an unprotected page.
+     */
+    public static final String LOGOUT_PAGE = "/logoutpage";
+
+    /**
+     * Name of the GET request parameter which holds the actual URI when
+     * the login page is shown to forward to that URI upon successful login. 
+     */
+    public static final String TARGET_URI_PARAM = "targetUri";
+
+    /**
+     * This is a) the challenge for the authentication and b) the string that
+     * is displayed in the standard HTTP login dialog. Configurable. 
+     */
+    private String realm_;
+    
     /** The logger. */
     private Logger log_;
 
     /** Root Cocoon Bean Factory. */
     private BeanFactory beanFactory_;
-    
-    private String realm_;
     
     /**
      * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
@@ -65,6 +138,11 @@ public class AuthenticationFilter implements Filter {
     }
 
     /**
+     * Filter method that implements the actual logic and decides whether to
+     * process by calling the next filter in the chain (another filter or the
+     * servlet) or stopping here and sending a special response (authentication
+     * request or redirect).
+     *  
      * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
      */
     public void doFilter(ServletRequest servletRequest, 
@@ -75,65 +153,115 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         
-        
-        // always send an authentication request 
-        // in order to avoid one round trip 
+        // always send an authentication request in order to avoid one round trip 
         response.setHeader("WWW-Authenticate", "BASIC realm=\""+ realm_ +"\"");
         
         String targetUri = getTargetURI(request);
         
+        // only do authentication for protected URIs, eg. excluded login page
         if (isProtected(targetUri)) {
             String authenticatedUser = authenticateUser(request);
+            
+            // the special login request is done to actually perform the
+            // authentication, this is typically the second step initiated by
+            // the login page
             if (isLoginRequest(request)) {
                 if (authenticatedUser == null) {
-                    // not authenticated.
+                    // not authenticated. trigger auth. with username / password
+                    // either by the HTTP auth dialog in the browser or
+                    // automatically by Javascript XMLHttpRequest
                     response.sendError(HttpServletResponse.SC_UNAUTHORIZED);                    
                 } else {
                     // authenticated. redirect to original target
-                    String originalUrl = request.getParameter("targetUri");
+                    String originalUrl = request.getParameter(TARGET_URI_PARAM);
                     String redirectUrl = response.encodeRedirectURL(
                             (originalUrl != null ? originalUrl : ""));
                     response.sendRedirect(redirectUrl);
                 }
+                
+                // no further servlet processing.
                 return;
+                
             }
             else {
+                // 99 percent of all pages, not the special login request.
+                
+                // here we either have the first request to the server, ie.
+                // not yet authenticated, or some client that does not send the
+                // authorization data preemptively (although he already did
+                // authenticate) -> see isGuiBrowserRequest()
                 if (authenticatedUser == null) {
                     // not authenticated.
+                    
+                    // standard browser with preemptive sending auth data, thus
+                    // it must be the first request -> go to login page
                     if (isGuiBrowserRequest(request)) {
                         String redirectUrl = response.encodeRedirectURL(
-                                "/loginpage?" + "targetUri=" + targetUri);
+                                LOGIN_PAGE + "?" + TARGET_URI_PARAM + "=" + targetUri);
                         response.sendRedirect(redirectUrl);
                     } else {
+                        // trigger simple client auth. or re-authentication
                         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
                     }
+
+                    // no further servlet processing.
                     return;
+                    
                 } else {
                     // authenticated. make username available as request attribute
-                    request.setAttribute("username", authenticatedUser);
+                    request.setAttribute(USERNAME_ATTR, authenticatedUser);
                 }
             }
         }
+        
+        // access granted, proceed with the servlet
         chain.doFilter(servletRequest, servletResponse);
     }
     
+    /**
+     * Tests if the request is a special login request, used by the
+     * XMLHttpRequest of the Java-based login form.
+     */
     private boolean isLoginRequest(HttpServletRequest request) {
         String targetUri = getTargetURI(request);
-        return targetUri.equals("/") && 
-            "login".equals(request.getParameter("request"));
+        return targetUri.equals(LOGIN_REQUEST_URI) && 
+            LOGIN_REQUEST_VALUE.equals(request.getParameter(LOGIN_REQUEST_PARAM));
     }
     
+    /**
+     * Tests if the request comes from a popular GUI browser.
+     * 
+     * <p>
+     * This includes all the standard ones (Internet Explorer, Firefox, Safari,
+     * Opera, and all Mozilla-based) but excludes the non-popular and more
+     * strictly-following-the-RFC ones (like lynx or link). Those strict ones do
+     * not sent the Authorization header preemptively to all pages on the same
+     * server once there was an authorization. Thus it is not possible to decide
+     * whether that request is the first one (not yet logged in) and the login
+     * page should be displayed or if it is from that strict browser, that
+     * actually holds the credentials but does not send them yet without
+     * explicitly being asked for by sending yet another authorization
+     * challenge.
+     * </p>
+     * 
+     * <p>
+     * The check uses the Accept and the User-Agent header and is for most
+     * cases somewhat verbose, since the check for the User-Agent is sufficient.
+     * </p>
+     */
     private boolean isGuiBrowserRequest(HttpServletRequest request) {
         boolean result = true;
         String accept = request.getHeader("Accept");
         String userAgent = request.getHeader("User-Agent");
         
         if (accept != null) {
+            // first check for human-based browser content requests
             result = accept.contains("text/html") 
                 || accept.contains("application/xhtml+xml")
                 || accept.contains("*/*"); // e.g. for IE 6
             
             if (result && userAgent != null) {
+                // second check for popular browsers
                 result = userAgent.contains("MSIE")
                     || userAgent.contains("Gecko")
                     || userAgent.contains("Opera")
@@ -143,14 +271,32 @@ public class AuthenticationFilter implements Filter {
         return result;
     }
     
+    /**
+     * Checks if the requested URI is actually protected. Non-protected URIs are
+     * the login and logout pages as well as stylesheets, scripts and images for
+     * those pages.
+     */
     private boolean isProtected(String targetUri) {
-        // images might contain protected content (user photos, diagrams)
-        if (targetUri.matches("/loginpage(.)*$|/logoutpage$|(.)*\\.(css|js)$|/[^/]+\\.(png|jpg|gif)$"))
+        // (1) The login page is allowed.
+        
+        // (2) The logout page is allowed.
+
+        // (3) All css stylesheets and scripts are allowed.
+        
+        // (4) Images with the pattern "/header.png" are allowed, since they
+        //     belong to the login/logout page.
+        
+        // Other images might contain protected content (photos or diagrams)
+        
+        if (targetUri.matches(LOGIN_PAGE + "(.)*$|" + LOGOUT_PAGE + "(.)*$|(.)*\\.(css|js)$|/[^/]+\\.(png|jpg|gif)$"))
             return false;
         else
             return true;
     }
     
+    /**
+     * Returns the decoded authorization data from the HTTP header.
+     */
     private String[] authTupleFromAuthHeader(String authHeader) {
         
         String encodedAuthRequest = authHeader.substring(6);
@@ -159,6 +305,10 @@ public class AuthenticationFilter implements Filter {
         return new String(authRequest).split(":");
     }
     
+    /**
+     * This does the actual authentication using the teamspace authentication
+     * component.
+     */
     private String authenticateUser(HttpServletRequest request) {
         
         String authHeader = request.getHeader("Authorization");
@@ -189,6 +339,7 @@ public class AuthenticationFilter implements Filter {
         return (Authentication) beanFactory_.getBean(lookupName);
     }
 
+    /** TODO: this does not handle servlet contexts in a container! */
     protected String getTargetURI(HttpServletRequest request) {
 
         // get the part after http://host:port
--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/login.js
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/login.js
@@ -1,39 +1,8 @@
-/*function logout(target) {
-	try{
-	  var agt=navigator.userAgent.toLowerCase();
-	  if (agt.indexOf("msie") != -1) {
-	    // IE clear HTTP Authentication
-	    document.execCommand("ClearAuthenticationCache");
-	  } else {
-	    var xmlhttp = createXMLObject();
-	    xmlhttp.open("GET",target.href,false,"nouser","nopassword");
-	    xmlhttp.send("");
-	    xmlhttp.abort();
-	  }
-	//  window.location = "/rest/";
-	} catch(e) {
-		// There was an error
-		alert("there was an error");
-	}
-	return false;
-}*/
-
-function createXMLObject() {
-	try {
-		if (window.XMLHttpRequest) {
-			xmlhttp = new XMLHttpRequest();
-		}
-		// code for IE
-		else if (window.ActiveXObject) {
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-	} catch (e) {
-		xmlhttp=false
-	}
-	return xmlhttp;
-}
-
-dojo.addOnLoad(function()
+/**
+ * Copyright (C) 2006 Mindquarry GmbH, All Rights Reserved
+ */
+ 
+ dojo.addOnLoad(function()
 {	
 	var index = -1;
     var anchors = document.getElementsByTagName("a");
@@ -54,7 +23,7 @@ dojo.addOnLoad(function()
         }
     }
     if (index >= 0) {
-    	createLogoutForm(anchors[index]);
+    	createLogoutLink(anchors[index]);
     }
 });
 
@@ -66,41 +35,83 @@ function createLoginForm(httpauth)
     form.onsubmit = function() {login(form); return false;};
     form.id = httpauth.id;
 
-    var username = document.createElement("label");
+    var table = document.createElement("table");
+    table.id = httpauth.id + "-table";
+    
+    // IE needs a tBody element to recognize the table created here!
+    var tBody = document.createElement("tBody");
+    table.appendChild(tBody);
+
+    var row1 = document.createElement("tr");
+    tBody.appendChild(row1);
+    var cell11 = document.createElement("td");
+    row1.appendChild(cell11);
+    var cell12 = document.createElement("td");
+    row1.appendChild(cell12);
+
+    var row2 = document.createElement("tr");
+    tBody.appendChild(row2);
+    var cell21 = document.createElement("td");
+    row2.appendChild(cell21);
+    var cell22 = document.createElement("td");
+    row2.appendChild(cell22);
+
+    var row3 = document.createElement("tr");
+    tBody.appendChild(row3);
+    var cell31 = document.createElement("td");
+    row3.appendChild(cell31);
+    var cell32 = document.createElement("td");
+    row3.appendChild(cell32);
+
+    var usernameLabel = document.createElement("label");
+    usernameLabel.htmlFor = httpauth.id + "-username";
+    usernameLabel.appendChild(document.createTextNode("Username"));
+
     var usernameInput = document.createElement("input");
     usernameInput.name = "username";
     usernameInput.type = "text";
     usernameInput.id = httpauth.id + "-username";
-    username.appendChild(document.createTextNode("Username"));
-    username.appendChild(usernameInput);
     
-    var password = document.createElement("label");
+    var passwordLabel = document.createElement("label");
+    passwordLabel.htmlFor = httpauth.id + "-password";
+    passwordLabel.appendChild(document.createTextNode("Password"));
+
     var passwordInput = document.createElement("input");
     passwordInput.name = "password";
     passwordInput.type = "password";
     passwordInput.id = httpauth.id + "-password";
-    password.appendChild(document.createTextNode("Password"));
-    password.appendChild(passwordInput);
     
     var submit = document.createElement("input");
     submit.type = "submit";
     submit.value = "Log in";
     submit.id = httpauth.id + "-submit";
-    form.appendChild(username);
-    form.appendChild(password);
-    form.appendChild(submit);
+
+    cell11.appendChild(usernameLabel);
+    cell12.appendChild(usernameInput);
+
+    cell21.appendChild(passwordLabel);
+    cell22.appendChild(passwordInput);
+    
+    cell32.appendChild(submit);
+
+    form.appendChild(table);
 
     httpauth.parentNode.replaceChild(form, httpauth);
 }
 
-function createLogoutForm(httplogout)
+function createLogoutLink(httplogout)
 {
+/*
 	var logoutDiv = document.createElement("div");
-    logoutDiv.onclick = logout;
+    logoutDiv.onclick = function() {logout();};
     logoutDiv.id = "http-logout-link";
 	logoutDiv.innerHTML = "Log out";
 	
     httplogout.appendChild(logoutDiv);
+*/
+    httplogout.onclick = function() {logout();};
+    httplogout.id = "http-logout-link";
+	httplogout.innerHTML = "Log out";
 }
 
 function getHTTPObject() {
@@ -134,10 +145,17 @@ function login(form)
     var password = document.getElementById(form.id + "-password").value;
 
     var http = getHTTPObject();
+    
+    // call the special login request page provided by the server that
+    // does the authentication and will store the correct credentials in the
+    // cache of the browser
     http.open("get", "/?request=login", false, username, password);    
     http.send(null);
     
+    // we are not interested in the response, only the status
+    
 	if (http.status == 200) {
+		// go to the page that was requested before the login page came
 		document.location = form.action.split("targetUri=")[1];
 	} else {
         alert("Incorrect username and/or password!");
@@ -147,8 +165,12 @@ function login(form)
 function logout()
 {
     var http = getHTTPObject();
+    
+    // do a authentication with wrong username / password so that after the
+    // not-authorized response from the server the browser will clear those
+    // credentials from the cache => we are "logged out"
     http.open("get", "/", false, "null", "null");
     http.send(null);
-    document.location = "/logoutpage";
-    return false;
+    
+    document.location = "/logoutpage?targetUri=" + window.location.pathname;
 }
