b3ce7efe49ca9ee89ce6294a72e2a508b85d327d
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-15 14:54:54 +0000

    refined interface

--- trunk/mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/filters/TextFilter.java
+++ trunk/mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/filters/TextFilter.java
@@ -1,9 +1,15 @@
 package com.mindquarry.search.cocoon.filters;
 
 import java.io.InputStream;
+import java.io.Reader;
 import java.util.Map;
 
 public interface TextFilter {
-	boolean canFilter(String mimetype);
-	Map doFilter(InputStream stream);
+	/**
+	 * The keys of the returned map are the field names, the values are
+	 * Readers containing the content
+	 * @param stream the input stream to filter
+	 * @return the extracted data
+	 */
+	Map<String, Reader> doFilter(InputStream stream);
 }
