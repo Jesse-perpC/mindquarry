762893b23f586b8db5ae8c24628b59c0c76fc8c4
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-15 14:52:04 +0000

    lookup filter and run

--- trunk/mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/TextFilterGenerator.java
+++ trunk/mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/TextFilterGenerator.java
@@ -4,59 +4,81 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
+import org.apache.avalon.framework.service.ServiceException;
 import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.generation.FileGenerator;
 import org.apache.cocoon.generation.Generator;
-import org.xml.sax.helpers.AttributesImpl;
+import org.apache.excalibur.source.SourceNotFoundException;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
 
 import com.mindquarry.search.cocoon.filters.TextFilter;
 
 public class TextFilterGenerator extends FileGenerator implements Generator {
-	private List textFilters;
-	
+	private Map textFilters;
+
 	public void generate() throws IOException, SAXException,
 			ProcessingException {
 		Map contents = filter();
 		this.contentHandler.startDocument();
 		AttributesImpl atts = new AttributesImpl();
-		atts.addAttribute(null, "mime-type", "mime-type", "CDATA", this.inputSource.getMimeType());
-		this.contentHandler.startElement(null, "document", "document", new AttributesImpl());
-		for (Iterator i=contents.keySet().iterator();i.hasNext();) {
+		atts.addAttribute(null, "mime-type", "mime-type", "CDATA",
+				this.inputSource.getMimeType());
+		this.contentHandler.startElement(null, "document", "document",
+				new AttributesImpl());
+		for (Iterator i = contents.keySet().iterator(); i.hasNext();) {
 			Object key = i.next();
 			Reader value = (Reader) contents.get(key);
 			atts = new AttributesImpl();
-			//atts.addAttribute(null, "text-filter", "text-filter", "CDATA", value)
-			this.contentHandler.startElement(null, key.toString(), key.toString(), atts);
-			
+			// atts.addAttribute(null, "text-filter", "text-filter", "CDATA",
+			// value)
+			this.contentHandler.startElement(null, key.toString(), key
+					.toString(), atts);
+
 			int read = 0;
 			char[] cont = {};
-			
-			while((read = value.read(cont, 0, 1024))!=-1) {
+
+			while ((read = value.read(cont, 0, 1024)) != -1) {
 				this.contentHandler.characters(cont, 0, read);
 			}
-			
-			this.contentHandler.endElement(null, key.toString(), key.toString());
+
+			this.contentHandler
+					.endElement(null, key.toString(), key.toString());
 		}
 		this.contentHandler.endElement(null, "document", "document");
 		this.contentHandler.endDocument();
 	}
-	
+
 	private Map filter() {
-		for (final Iterator i=this.textFilters.iterator();i.hasNext();) {
-			final TextFilter filter = (TextFilter) i.next();
-			if (filter.canFilter(this.inputSource.getMimeType())) {
-				try {
-					return filter.doFilter(this.inputSource.getInputStream());
-				} catch (final Exception e) {
-					getLogger().error("Failed to filter source", e);
-				}
+		TextFilter filter = (TextFilter) this.textFilters.get(this.inputSource
+				.getMimeType());
+		if (filter == null) {
+			try {
+				filter = (TextFilter) this.manager
+						.lookup("com.mindquarry.search.TextFilter/"
+								+ this.inputSource.getMimeType());
+				this.textFilters.put(this.inputSource.getMimeType(), filter);
+
+				return filter.doFilter(this.inputSource.getInputStream());
+			} catch (ServiceException e) {
+				getLogger().error("Could not lookup filter", e);
+			} catch (SourceNotFoundException e) {
+				getLogger().error("Document not found", e);
+			} catch (IOException e) {
+				getLogger().error("Could not read document", e);
 			}
 		}
 		return new HashMap();
+	}
+
+	public void dispose() {
+		super.dispose();
+		for (Iterator i = this.textFilters.values().iterator(); i.hasNext();) {
+			this.manager.release(i.next());
+		}
+		this.textFilters.clear();
 	}
 
 }
