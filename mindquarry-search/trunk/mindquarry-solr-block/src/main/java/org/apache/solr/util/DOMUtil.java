18013d1a87e29a157e175fbc7063b1e244257b22
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-15 18:19:01 +0000

    fixed bug in config parsing functions (Node.getTextContent()) does not work with older Xerces versions)

--- mindquarry-search/trunk/mindquarry-solr-block/src/main/java/org/apache/solr/util/DOMUtil.java
+++ mindquarry-search/trunk/mindquarry-solr-block/src/main/java/org/apache/solr/util/DOMUtil.java
@@ -17,6 +17,7 @@
 
 package org.apache.solr.util;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -32,122 +33,150 @@ import java.util.ArrayList;
  */
 public class DOMUtil {
 
-  public static Map<String,String> toMap(NamedNodeMap attrs) {
-    return toMapExcept(attrs);
-  }
-
-  public static Map<String,String> toMapExcept(NamedNodeMap attrs, String... exclusions) {
-    Map<String,String> args = new HashMap<String,String>();
-    outer: for (int j=0; j<attrs.getLength(); j++) {
-      Node attr = attrs.item(j);
-      String attrName = attr.getNodeName();
-      for (String ex : exclusions)
-        if (ex.equals(attrName)) continue outer;
-      String val = attr.getNodeValue();
-      args.put(attrName, val);
-    }
-    return args;
-  }
-
-  public static Node getChild(Node node, String name) {
-    if (!node.hasChildNodes()) return null;
-    NodeList lst = node.getChildNodes();
-    if (lst == null) return null;
-    for (int i=0; i<lst.getLength(); i++) {
-      Node child = lst.item(i);
-      if (name.equals(child.getNodeName())) return child;
-    }
-    return null;
-  }
-
-  public static String getAttr(NamedNodeMap attrs, String name) {
-    return getAttr(attrs,name,null);
-  }
-
-  public static String getAttr(Node nd, String name) {
-    return getAttr(nd.getAttributes(), name);
-  }
-
-  public static String getAttr(NamedNodeMap attrs, String name, String missing_err) {
-    Node attr = attrs==null? null : attrs.getNamedItem(name);
-    if (attr==null) {
-      if (missing_err==null) return null;
-      throw new RuntimeException(missing_err + ": missing mandatory attribute '" + name + "'");
-    }
-    String val = attr.getNodeValue();
-    return val;
-  }
-
-  public static String getAttr(Node node, String name, String missing_err) {
-    return getAttr(node.getAttributes(), name, missing_err);
-  }
-
-  //////////////////////////////////////////////////////////
-  // Routines to parse XML in the syntax of the Solr query
-  // response schema.
-  // Should these be moved to Config?  Should all of these things?
-  //////////////////////////////////////////////////////////
-  public static NamedList childNodesToNamedList(Node nd) {
-    return nodesToNamedList(nd.getChildNodes());
-  }
-
-  public static List childNodesToList(Node nd) {
-    return nodesToList(nd.getChildNodes());
-  }
-
-  public static NamedList nodesToNamedList(NodeList nlst) {
-    NamedList clst = new NamedList();
-    for (int i=0; i<nlst.getLength(); i++) {
-      addToNamedList(nlst.item(i), clst, null);
-    }
-    return clst;
-  }
-
-  public static List nodesToList(NodeList nlst) {
-    List lst = new ArrayList();
-    for (int i=0; i<nlst.getLength(); i++) {
-      addToNamedList(nlst.item(i), null, lst);
-    }
-    return lst;
-  }
-
-
-  public static void addToNamedList(Node nd, NamedList nlst, List arr) {
-    // Nodes often include whitespace, etc... so just return if this
-    // is not an Element.
-    if (nd.getNodeType() != Node.ELEMENT_NODE) return;
-
-    String type = nd.getNodeName();
-
-    String name = null;
-    if (nd.hasAttributes()) {
-      NamedNodeMap attrs = nd.getAttributes();
-      Node nameNd = attrs.getNamedItem("name");
-      if (nameNd != null) name=nameNd.getNodeValue();
+    public static Map<String, String> toMap(NamedNodeMap attrs) {
+        return toMapExcept(attrs);
     }
 
-    Object val=null;
-
-    if ("str".equals(type)) {
-      val = nd.getTextContent();
-    } else if ("int".equals(type)) {
-      val = Integer.valueOf(nd.getTextContent());
-    } else if ("long".equals(type)) {
-      val = Long.valueOf(nd.getTextContent());
-    } else if ("float".equals(type)) {
-      val = Float.valueOf(nd.getTextContent());
-    } else if ("double".equals(type)) {
-      val = Double.valueOf(nd.getTextContent());
-    } else if ("bool".equals(type)) {
-      val = Boolean.valueOf(nd.getTextContent());
-    } else if ("lst".equals(type)) {
-      val = childNodesToNamedList(nd);
-    } else if ("arr".equals(type)) {
-      val = childNodesToList(nd);
+    public static Map<String, String> toMapExcept(NamedNodeMap attrs,
+            String... exclusions) {
+        Map<String, String> args = new HashMap<String, String>();
+        outer: for (int j = 0; j < attrs.getLength(); j++) {
+            Node attr = attrs.item(j);
+            String attrName = attr.getNodeName();
+            for (String ex : exclusions)
+                if (ex.equals(attrName))
+                    continue outer;
+            String val = attr.getNodeValue();
+            args.put(attrName, val);
+        }
+        return args;
     }
 
-    if (nlst != null) nlst.add(name,val);
-    if (arr != null) arr.add(val);
-  }
+    public static Node getChild(Node node, String name) {
+        if (!node.hasChildNodes())
+            return null;
+        NodeList lst = node.getChildNodes();
+        if (lst == null)
+            return null;
+        for (int i = 0; i < lst.getLength(); i++) {
+            Node child = lst.item(i);
+            if (name.equals(child.getNodeName()))
+                return child;
+        }
+        return null;
+    }
 
+    public static String getAttr(NamedNodeMap attrs, String name) {
+        return getAttr(attrs, name, null);
+    }
+
+    public static String getAttr(Node nd, String name) {
+        return getAttr(nd.getAttributes(), name);
+    }
+
+    public static String getAttr(NamedNodeMap attrs, String name,
+            String missing_err) {
+        Node attr = attrs == null ? null : attrs.getNamedItem(name);
+        if (attr == null) {
+            if (missing_err == null)
+                return null;
+            throw new RuntimeException(missing_err
+                    + ": missing mandatory attribute '" + name + "'");
+        }
+        String val = attr.getNodeValue();
+        return val;
+    }
+
+    public static String getAttr(Node node, String name, String missing_err) {
+        return getAttr(node.getAttributes(), name, missing_err);
+    }
+
+    // ////////////////////////////////////////////////////////
+    // Routines to parse XML in the syntax of the Solr query
+    // response schema.
+    // Should these be moved to Config? Should all of these things?
+    // ////////////////////////////////////////////////////////
+    public static NamedList childNodesToNamedList(Node nd) {
+        return nodesToNamedList(nd.getChildNodes());
+    }
+
+    public static List childNodesToList(Node nd) {
+        return nodesToList(nd.getChildNodes());
+    }
+
+    public static NamedList nodesToNamedList(NodeList nlst) {
+        NamedList clst = new NamedList();
+        for (int i = 0; i < nlst.getLength(); i++) {
+            addToNamedList(nlst.item(i), clst, null);
+        }
+        return clst;
+    }
+
+    public static List nodesToList(NodeList nlst) {
+        List lst = new ArrayList();
+        for (int i = 0; i < nlst.getLength(); i++) {
+            addToNamedList(nlst.item(i), null, lst);
+        }
+        return lst;
+    }
+
+    public static void addToNamedList(Node nd, NamedList nlst, List arr) {
+        // Nodes often include whitespace, etc... so just return if this
+        // is not an Element.
+        if (nd.getNodeType() != Node.ELEMENT_NODE)
+            return;
+
+        String type = nd.getNodeName();
+
+        String name = null;
+        if (nd.hasAttributes()) {
+            NamedNodeMap attrs = nd.getAttributes();
+            Node nameNd = attrs.getNamedItem("name");
+            if (nameNd != null)
+                name = nameNd.getNodeValue();
+        }
+
+        Object val = null;
+
+        if ("str".equals(type)) {
+            val = getText(nd);
+        } else if ("int".equals(type)) {
+            val = Integer.valueOf(getText(nd));
+        } else if ("long".equals(type)) {
+            val = Long.valueOf(getText(nd));
+        } else if ("float".equals(type)) {
+            val = Float.valueOf(getText(nd));
+        } else if ("double".equals(type)) {
+            val = Double.valueOf(getText(nd));
+        } else if ("bool".equals(type)) {
+            val = Boolean.valueOf(getText(nd));
+        } else if ("lst".equals(type)) {
+            val = childNodesToNamedList(nd);
+        } else if ("arr".equals(type)) {
+            val = childNodesToList(nd);
+        }
+
+        if (nlst != null)
+            nlst.add(name, val);
+        if (arr != null)
+            arr.add(val);
+    }
+
+    public static String getText(Node nd) {
+        String txt = null;
+        if (nd.getNodeType() == Element.ELEMENT_NODE) {
+            StringBuffer sb = new StringBuffer();
+
+            NodeList childs = nd.getChildNodes();
+            for (int i = 0; i < childs.getLength(); i++) {
+                Node child = childs.item(i);
+                sb.append(child.getNodeValue());
+            }
+            sb.trimToSize();
+            txt = sb.toString();
+        } else {
+            txt = nd.getNodeValue();
+        }
+        return txt;
+    }
 }
