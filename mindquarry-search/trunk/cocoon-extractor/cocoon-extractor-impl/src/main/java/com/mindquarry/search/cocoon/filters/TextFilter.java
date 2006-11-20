aca67c8ac992c21d862bcb4f686570188549f274
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-20 18:34:27 +0000

    documented TextFilterGenerator reviewed, cleaned up and fixed code added FilterException for TextFilter interface moved specific implementation SlideTextFilter into impl package

--- mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/filters/TextFilter.java
+++ mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/filters/TextFilter.java
@@ -1,3 +1,6 @@
+/*
+ * Coypright (c) 2006 Mindquarry GmbH, Potsdam, Germany 
+ */
 package com.mindquarry.search.cocoon.filters;
 
 import java.io.InputStream;
@@ -9,9 +12,10 @@ public interface TextFilter {
 	 * Use this for the full text
 	 */
 	public static final String CONTENT = "content";
-	public static final String TITLE = "title";
-	public static final String ABSTRACT = "abstract";
-	public static final String AUTHOR = "author";
+    
+//	public static final String TITLE = "title";
+//	public static final String ABSTRACT = "abstract";
+//	public static final String AUTHOR = "author";
 	
 	/**
 	 * The keys of the returned map are the field names, the values are
@@ -19,5 +23,5 @@ public interface TextFilter {
 	 * @param stream the input stream to filter
 	 * @return the extracted data
 	 */
-	Map<String, Reader> doFilter(InputStream stream) throws Exception;
+	Map<String, Reader> doFilter(InputStream stream) throws FilterException;
 }
