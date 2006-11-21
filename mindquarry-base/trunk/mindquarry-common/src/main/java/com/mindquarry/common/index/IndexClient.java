4df356d46115c5da2f99adc278c00d71ad31f6c5
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-21 10:34:58 +0000

    renamed indexing components to indexing client

--- mindquarry-base/trunk/mindquarry-common/src/main/java/com/mindquarry/common/index/IndexClient.java
+++ mindquarry-base/trunk/mindquarry-common/src/main/java/com/mindquarry/common/index/IndexClient.java
@@ -11,8 +11,8 @@ import java.io.InputStream;
  * @author <a href="mailto:alexander(dot)saar(at)mindquarry(dot)com">Alexander
  *         Saar</a>
  */
-public interface Indexer {
-    public static final String ROLE = Indexer.class.getName();
+public interface IndexClient {
+    public static final String ROLE = IndexClient.class.getName();
     
     public void index(InputStream content, String name, String location,
             String type);
