0b0cdeb27773bbe4d71eeb401f71ef2f04777522
jeremy <jeremy@b115fd6e-e050-0410-b27a-7792256590da>
2006-11-30 16:58:32 +0000

    namespaced the javascript

--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/wikiLink.js
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/wikiLink.js
@@ -2,14 +2,21 @@
 dojo.provide("mindquarry.widget.wikiLink");
 dojo.require("mindquarry.widget.normalizeName");
 
+
+
+// setup the namespace
+var mindquarry = mindquarry || {};
+mindquarry.widget = mindquarry.widget || {};
+
+
 // makes a link out of the selection in the Wiki Editor, from a list of available wiki pages in this teamspace
-function wikiLinkListClick() {			
-	var _editor = dojo.widget.byType("Editor2")[0];	
+mindquarry.widget.wikiLinkListClick = function() {			
+	var editor = dojo.widget.byType("Editor2")[0];	
 	var range;
-	if(_editor.document.selection){
-		range = _editor.document.selection.createRange().text;
+	if(editor.document.selection){
+		range = editor.document.selection.createRange().text;
 	}else if(dojo.render.html.mozilla){
-		range = _editor.window.getSelection().toString();
+		range = editor.window.getSelection().toString();
 	}
 	if(range.length){
 		var wikiLinkDialog = dojo.widget.byId("wikiLinkDialog");
@@ -24,25 +31,25 @@ function wikiLinkListClick() {
 }
 
 // makes a link out of the selection in the Wiki Editor, using the selection text as the href
-function wikiLinkWordClick() {			
-	var _editor = dojo.widget.byType("Editor2")[0];	
+mindquarry.widget.wikiLinkWordClick = function() {			
+	var editor = dojo.widget.byType("Editor2")[0];	
 	var range;
-	if(_editor.document.selection){
-		range = _editor.document.selection.createRange().text;
+	if(editor.document.selection){
+		range = editor.document.selection.createRange().text;
 	}else if(dojo.render.html.mozilla){
-		range = _editor.window.getSelection().toString();
+		range = editor.window.getSelection().toString();
 	}
 	if(range.length){
-		wikiLinkSet(range);
+		this.wikiLinkSet(range);
 	}else{
 		alert("Please select text to link");
 	}
 }
 
-function wikiLinkSet(href) {
-	var _editorToolbar = dojo.widget.byType("Editor2Toolbar")[0];
-	href = normalizeName(href);
+mindquarry.widget.wikiLinkSet = function(href) {
+	var editorToolbar = dojo.widget.byType("Editor2Toolbar")[0];
+	href = this.normalizeName(href);
 	if (href) {
-		_editorToolbar.exec("createlink", href);
+		editorToolbar.exec("createlink", href);
 	}
 }
