fceac102b8cddf18ee5ca9b4e9e327df145e5d96
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-01 11:30:07 +0000

    added title extraction for xml and as fallback returning the filename of the source

--- mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/filters/impl/SlideTextFilter.java
+++ mindquarry-search/trunk/cocoon-extractor/cocoon-extractor-impl/src/main/java/com/mindquarry/search/cocoon/filters/impl/SlideTextFilter.java
@@ -5,11 +5,15 @@ package com.mindquarry.search.cocoon.filters.impl;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.excalibur.source.Source;
 import org.apache.excalibur.source.SourceNotFoundException;
+import org.apache.excalibur.source.TraversableSource;
 import org.apache.slide.common.Domain;
 import org.apache.slide.extractor.ContentExtractor;
 import org.apache.slide.extractor.ExtractorException;
@@ -39,6 +43,9 @@ public class SlideTextFilter implements TextFilter {
             }
             if (this.titleExtractor != null) {
                 contents.put(TextFilter.TITLE, this.titleExtractor.extract(source.getInputStream()));
+            } else {
+                // fallback, use title from the path
+                contents.put(TextFilter.TITLE, new StringReader(getTitleFromFilename(source)));
             }
         } catch (ExtractorException e) {
             throw new FilterException(e);
@@ -50,4 +57,25 @@ public class SlideTextFilter implements TextFilter {
 		return contents;
 	}
 
+    private String getTitleFromFilename(Source source) {
+        String filename;
+        if (source instanceof TraversableSource) {
+            filename = ((TraversableSource) source).getName();
+        } else {
+            try {
+                URL url = new URL(source.getURI());
+                filename = url.getFile();
+            } catch (MalformedURLException e) {
+                return "";
+            }
+        }
+        
+        // the filename might have an extension, remove it
+        int dotPos = filename.lastIndexOf("."); 
+        if (dotPos >= 0) {
+            filename = filename.substring(0, dotPos);
+        }
+
+        return filename;
+    }
 }
