a819481908f3039f0acf2a052d596e5c7ca76027
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-01 10:01:37 +0000

    added exceptions to create and remove teamspace listeners

--- trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/main/java/com/mindquarry/teamspace/manager/TeamspaceManager.java
+++ trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/main/java/com/mindquarry/teamspace/manager/TeamspaceManager.java
@@ -21,6 +21,8 @@ import com.mindquarry.common.init.InitializationException;
 import com.mindquarry.common.persistence.Session;
 import com.mindquarry.common.persistence.SessionFactory;
 import com.mindquarry.teamspace.Authorisation;
+import com.mindquarry.teamspace.CouldNotCreateTeamspaceException;
+import com.mindquarry.teamspace.CouldNotRemoveTeamspaceException;
 import com.mindquarry.teamspace.Membership;
 import com.mindquarry.teamspace.TeamspaceAdmin;
 import com.mindquarry.teamspace.TeamspaceDefinition;
@@ -32,38 +34,39 @@ import com.mindquarry.user.manager.UserManager;
 
 /**
  * Add summary documentation here.
- *
- * @author 
- * <a href="mailto:bastian.steinert(at)mindquarry.com">Bastian Steinert</a>
+ * 
+ * @author <a href="mailto:bastian.steinert(at)mindquarry.com">Bastian Steinert</a>
  */
 public final class TeamspaceManager implements TeamspaceAdmin, Authorisation {
-    
+
     static final String ADMIN_USER_ID = "admin";
+
     static final String ADMIN_PWD = "admin";
+
     static final String ADMIN_NAME = "Administrator";
-    
-    /** helds the constructor of a dynamic generated proxy for 
-     * interface java.util.List. a proxy is used for 
-     * lazy loading of team members within a teamspace entity object.*/
+
+    /**
+     * helds the constructor of a dynamic generated proxy for interface
+     * java.util.List. a proxy is used for lazy loading of team members within a
+     * teamspace entity object.
+     */
     private static Constructor listProxyConstructor_;
-    
+
     static {
         prepareLazyListLoading();
     }
-    
+
     /**
-     * creates a proxy class for the java.util.List interface
-     * and stores the default constructor of this class in a member variable. 
+     * creates a proxy class for the java.util.List interface and stores the
+     * default constructor of this class in a member variable.
      */
     static private void prepareLazyListLoading() {
-        Class proxyClazz = Proxy.getProxyClass(
-                TeamspaceManager.class.getClassLoader(), 
-                new Class[] {List.class});
-        
-        Class[] constrParamType = new Class[] {InvocationHandler.class};
+        Class proxyClazz = Proxy.getProxyClass(TeamspaceManager.class
+                .getClassLoader(), new Class[] { List.class });
+
+        Class[] constrParamType = new Class[] { InvocationHandler.class };
         try {
-            listProxyConstructor_ = 
-                proxyClazz.getConstructor(constrParamType);
+            listProxyConstructor_ = proxyClazz.getConstructor(constrParamType);
         } catch (SecurityException e) {
             throw new InitializationException(
                     "could not get constructor method from dynamic proxy", e);
@@ -72,15 +75,14 @@ public final class TeamspaceManager implements TeamspaceAdmin, Authorisation {
                     "could not get constructor method from dynamic proxy", e);
         }
     }
-    
+
     private SessionFactory sessionFactory_;
-    
+
     private DefaultListenerRegistry listenerRegistry_;
-    
+
     private UserManager userManager_;
-    
+
     private AuthorizationAdmin authAdmin_;
-        
 
     /**
      * Setter for listenerRegistry bean, set by spring at object creation
@@ -95,7 +97,7 @@ public final class TeamspaceManager implements TeamspaceAdmin, Authorisation {
     public void setSessionFactory(SessionFactory sessionFactory) {
         sessionFactory_ = sessionFactory;
     }
-    
+
     /**
      * Setter for userManager bean, set by spring at object creation
      */
@@ -105,158 +107,172 @@ public final class TeamspaceManager implements TeamspaceAdmin, Authorisation {
 
     /**
      * Setter for authAdmin.
-     *
+     * 
      * @param authAdmin the authAdmin to set
      */
     public void setAuthAdmin(AuthorizationAdmin authAdmin) {
         authAdmin_ = authAdmin;
     }
 
-    public void initialize() {        
+    public void initialize() {
         prepareLazyListLoading();
-        
+
         for (TeamspaceRO teamspace : queryAllTeamspaces()) {
             GroupRO teamGroup = userManager_.groupById(teamspace.getId());
             if (teamGroup == null) {
                 teamGroup = userManager_.createGroup(teamspace.getId());
-                List<UserRO> teamMembers = 
-                    userManager_.queryMembersForTeamspace(teamspace.getId());
-                
+                List<UserRO> teamMembers = userManager_
+                        .queryMembersForTeamspace(teamspace.getId());
+
                 for (UserRO teamMember : teamMembers)
                     userManager_.addUser(teamMember, teamGroup);
-                
+
                 String teamspaceUri = "/teamspaces/" + teamspace.getId();
                 RightRO rRight = authAdmin_.createRight(teamspaceUri, "READ");
                 RightRO wRight = authAdmin_.createRight(teamspaceUri, "WRITE");
-                
+
                 String profileName = teamspace.getId() + "-user";
                 ProfileRO profile = authAdmin_.createProfile(profileName);
                 authAdmin_.addRight(rRight, profile);
                 authAdmin_.addRight(wRight, profile);
-                
+
                 authAdmin_.addAllowance(profile, teamGroup);
             }
         }
     }
 
-    public TeamspaceDefinition createTeamspace(String teamspaceId, 
-            String name, String description, UserRO teamspaceCreator) {
-        
+    public TeamspaceDefinition createTeamspace(String teamspaceId, String name,
+            String description, UserRO teamspaceCreator)
+            throws CouldNotCreateTeamspaceException {
+
         TeamspaceEntity teamspace = new TeamspaceEntity();
         teamspace.setId(teamspaceId);
         teamspace.setName(name);
         teamspace.setDescription(description);
-        
-        listenerRegistry_.signalBeforeTeamspaceCreated(teamspace);
-        
+
+        try {
+            listenerRegistry_.signalBeforeTeamspaceCreated(teamspace);
+        } catch (Exception e) {
+            throw new CouldNotCreateTeamspaceException(
+                    "Teamspace creation failed in listener: " + e.getMessage(),
+                    e);
+        }
+
         userManager_.addUserToTeamspace(teamspaceCreator, teamspace.getId());
-        
+
         Session session = currentSession();
         session.persist(teamspace);
         session.commit();
         return teamspace;
     }
-    
+
     public void updateTeamspace(TeamspaceDefinition teamspace) {
         Session session = currentSession();
         session.update(teamspace);
         session.commit();
     }
 
-    public void removeTeamspace(String teamspaceId) {
-        
+    public void removeTeamspace(String teamspaceId)
+            throws CouldNotRemoveTeamspaceException {
+
         Session session = currentSession();
         TeamspaceEntity teamspace = queryTeamspaceById(session, teamspaceId);
-        
+
         if (null == teamspace)
-            throw new TeamspaceException(
-                    "the teamspace " + teamspaceId + " does not exist."  );
-        
+            throw new TeamspaceException("the teamspace " + teamspaceId
+                    + " does not exist.");
+
         List<UserRO> users = queryMembersForTeamspace(teamspace);
         for (UserRO user : users)
             userManager_.removeUserFromTeamspace(user, teamspace.getId());
-        
+
+        try {
+            listenerRegistry_.signalAfterTeamspaceRemoved(teamspace);
+        } catch (Exception e) {
+            throw new CouldNotRemoveTeamspaceException(
+                    "Teamspace removal failed in listener " + e.getMessage(), e);
+        }
+
         session.delete(teamspace);
         session.commit();
-        
-        listenerRegistry_.signalAfterTeamspaceRemoved(teamspace);
     }
 
     public List<TeamspaceRO> teamspacesForUser(String userId) {
-        assert userManager_.isValidUserId(userId) : 
-            "the userId: " + userId + " is not valid";
-        
-        Session session = currentSession();        
+        assert userManager_.isValidUserId(userId) : "the userId: " + userId
+                + " is not valid";
+
+        Session session = currentSession();
         List<TeamspaceRO> result;
-        
+
         if (ADMIN_USER_ID.equals(userId))
             result = queryAllTeamspaces();
         else
             result = queryTeamspacesForUser(userId);
-        
+
         session.commit();
         return result;
     }
 
     private List<TeamspaceRO> queryAllTeamspaces() {
         Session session = currentSession();
-        
-        List<Object> queriedTeamspaces = session.query(
-                "getAllTeamspaces", new Object[0]);
-                
+
+        List<Object> queriedTeamspaces = session.query("getAllTeamspaces",
+                new Object[0]);
+
         List<TeamspaceRO> result = new LinkedList<TeamspaceRO>();
-        for (Object teamspaceObj : queriedTeamspaces) {            
+        for (Object teamspaceObj : queriedTeamspaces) {
             TeamspaceEntity teamspace = (TeamspaceEntity) teamspaceObj;
             teamspace.setUsers(createLazyLoadMembersList(teamspace));
             result.add(teamspace);
         }
-        
+
         session.commit();
         return result;
     }
-    
+
     private List<TeamspaceRO> queryTeamspacesForUser(String userId) {
         Session session = currentSession();
-        
+
         List<TeamspaceRO> result = new LinkedList<TeamspaceRO>();
-        
+
         UserRO user = userManager_.userById(userId);
         if (user != null) {
             for (String teamRef : user.teamspaces()) {
                 TeamspaceEntity teamspace = queryTeamspaceById(session, teamRef);
                 result.add(teamspace);
-            }            
+            }
         }
-        
+
         session.commit();
         return result;
     }
-    
+
     List<UserRO> queryMembersForTeamspace(TeamspaceRO teamspace) {
         return userManager_.queryMembersForTeamspace(teamspace.getId());
     }
-    
+
     /**
      * @returns a teamspace object if it can be found otherwise null
      */
     private TeamspaceEntity queryTeamspaceById(Session session, String id) {
         TeamspaceEntity result = null;
-        List queryResult = session.query("getTeamspaceById", new Object[] {id});
+        List queryResult = session.query("getTeamspaceById",
+                new Object[] { id });
         if (queryResult.size() == 1) {
             result = (TeamspaceEntity) queryResult.get(0);
             result.setUsers(createLazyLoadMembersList(result));
         }
         return result;
     }
-    
+
     private List<UserRO> createLazyLoadMembersList(TeamspaceRO teamspace) {
-        
+
         ListLoading<UserRO> loader = new MembersLoading(teamspace, this);
-        
+
         LazyLoadListInvocationHandler<UserRO> invocationHandler;
         invocationHandler = new LazyLoadListInvocationHandler<UserRO>(loader);
-        
-        Object[] params = new Object[] {invocationHandler};
+
+        Object[] params = new Object[] { invocationHandler };
         try {
             Object proxy = listProxyConstructor_.newInstance(params);
             return (List<UserRO>) proxy;
@@ -274,18 +290,18 @@ public final class TeamspaceManager implements TeamspaceAdmin, Authorisation {
                     "could not create lazyLoadList proxy", e);
         }
     }
-    
+
     private Session currentSession() {
         return sessionFactory_.currentSession();
     }
-    
+
     /**
      * @see com.mindquarry.teamspace.TeamspaceQuery#teamspaceForId(java.lang.String)
      */
     public TeamspaceRO teamspaceForId(String teamspaceId) {
         return teamspaceDefinitionForId(teamspaceId);
     }
-    
+
     /**
      * @see com.mindquarry.teamspace.TeamspaceAdmin#teamspaceDefinitionForId(java.lang.String)
      */
@@ -298,17 +314,17 @@ public final class TeamspaceManager implements TeamspaceAdmin, Authorisation {
         List<UserRO> users = userManager_.allUsers();
         Set<UserRO> members = new HashSet<UserRO>();
         Set<UserRO> nonMembers = new HashSet<UserRO>();
-        
+
         for (UserRO user : users) {
             if (user.isMemberOf(teamspace))
                 members.add(user);
             else
                 nonMembers.add(user);
         }
-        
+
         return new Membership(teamspace, members, nonMembers);
     }
-    
+
     public Membership refreshMembership(Membership oldMembership) {
         Membership result = membership(oldMembership.teamspace);
         for (UserRO user : oldMembership.getAddedMembers()) {
@@ -320,43 +336,42 @@ public final class TeamspaceManager implements TeamspaceAdmin, Authorisation {
         return result;
     }
 
-    
-    // TODO think about requerying or reattaching of users and teamspaces 
+    // TODO think about requerying or reattaching of users and teamspaces
     // to new session, because most of the times the object was queried
     // in transactions and sessions before the current one
     public Membership updateMembership(Membership membership) {
 
         String teamspaceId = membership.teamspace.getId();
-        
+
         // check old members for removal from teamspace
-        for (UserRO user : membership.getRemovedMembers())    
+        for (UserRO user : membership.getRemovedMembers())
             userManager_.removeUserFromTeamspace(user, teamspaceId);
-        
+
         // check new members for adding to teamspace
         for (UserRO user : membership.getAddedMembers())
             userManager_.addUserToTeamspace(user, teamspaceId);
-                
+
         // return a new calculated membership
         return membership(membership.teamspace);
     }
 
     /**
-     * @see com.mindquarry.teamspace.Authorisation#authorise(java.lang.String, java.lang.String, java.lang.String)
+     * @see com.mindquarry.teamspace.Authorisation#authorise(java.lang.String,
+     *      java.lang.String, java.lang.String)
      */
     public boolean authorise(String userId, String uri, String method) {
         // check if user is in teamspace
-        
+
         boolean mayPerform = false;
-        
+
         if (uri.equals("jcr:///teamspaces") || uri.equals("jcr:///users")) {
             mayPerform = userId.equals("admin");
-        }
-        else {
+        } else {
             // simple regular expression that looks for the teamspace name...
             Pattern p = Pattern.compile("jcr:///teamspaces/([^/]*)/(.*)");
             Matcher m = p.matcher(uri);
             if (m.matches()) {
-                String requestedTeamspaceID = m.group(1); //"mindquarry";
+                String requestedTeamspaceID = m.group(1); // "mindquarry";
                 // ...and checks if the user is in that teamspace
                 UserRO user = userManager_.userById(userId);
                 mayPerform = user.isMemberOf(requestedTeamspaceID);
--- trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/test/java/com/mindquarry/teamspace/manager/TeamspaceManagerTest.java
+++ trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/test/java/com/mindquarry/teamspace/manager/TeamspaceManagerTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.avalon.framework.service.ServiceException;
 
+import com.mindquarry.teamspace.CouldNotCreateTeamspaceException;
+import com.mindquarry.teamspace.CouldNotRemoveTeamspaceException;
 import com.mindquarry.teamspace.Membership;
 import com.mindquarry.teamspace.TeamspaceAdmin;
 import com.mindquarry.teamspace.TeamspaceAlreadyExistsException;
@@ -16,36 +18,38 @@ import com.mindquarry.user.UserAdmin;
 import com.mindquarry.user.UserRO;
 
 public class TeamspaceManagerTest extends TeamspaceTestBase {
-      
-	public void testCreateAndRemoveTeamspace() 
-        throws ServiceException, TeamspaceAlreadyExistsException {
-        
-        TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
-        UserAdmin userAdmin = lookupUserAdmin();
-        
-        String userId = "mindquarry-user";
-        UserRO creator = userAdmin.createUser(userId, "aSecretPassword", 
-                "Mindquarry User", "surname", "an email", "the skills");
-        
-        String teamspaceId = "mindquarry-teamspace";
-        teamsAdmin.createTeamspace(teamspaceId, "Mindquarry Teamspace", 
-                "a greate description", creator);
-        
-        List<TeamspaceRO> teamspaces = teamsAdmin.teamspacesForUser(userId);
-        assertEquals(1, teamspaces.size());
-        assertEquals(1, teamspaces.get(0).getUsers().size());
-        
-        teamsAdmin.removeTeamspace(teamspaceId);
-        
-        assertEquals(0, teamsAdmin.teamspacesForUser(userId).size());
-	}
-    
-    public void testCreateAndRemoveTeamspaceAsAdmin() throws ServiceException,
-            TeamspaceAlreadyExistsException {
+
+    public void testCreateAndRemoveTeamspace() throws ServiceException,
+            TeamspaceAlreadyExistsException, CouldNotCreateTeamspaceException,
+            CouldNotRemoveTeamspaceException {
 
         TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
         UserAdmin userAdmin = lookupUserAdmin();
-        
+
+        String userId = "mindquarry-user";
+        UserRO creator = userAdmin.createUser(userId, "aSecretPassword",
+                "Mindquarry User", "surname", "an email", "the skills");
+
+        String teamspaceId = "mindquarry-teamspace";
+        teamsAdmin.createTeamspace(teamspaceId, "Mindquarry Teamspace",
+                "a greate description", creator);
+
+        List<TeamspaceRO> teamspaces = teamsAdmin.teamspacesForUser(userId);
+        assertEquals(1, teamspaces.size());
+        assertEquals(1, teamspaces.get(0).getUsers().size());
+
+        teamsAdmin.removeTeamspace(teamspaceId);
+
+        assertEquals(0, teamsAdmin.teamspacesForUser(userId).size());
+    }
+
+    public void testCreateAndRemoveTeamspaceAsAdmin() throws ServiceException,
+            TeamspaceAlreadyExistsException, CouldNotCreateTeamspaceException,
+            CouldNotRemoveTeamspaceException {
+
+        TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
+        UserAdmin userAdmin = lookupUserAdmin();
+
         String userId = "admin";
         UserRO creator = userAdmin.userById(userId);
 
@@ -61,117 +65,117 @@ public class TeamspaceManagerTest extends TeamspaceTestBase {
 
         assertEquals(0, teamsAdmin.teamspacesForUser(userId).size());
     }
-    
-    public void testProperties() 
-        throws ServiceException, TeamspaceAlreadyExistsException {
-        
+
+    public void testProperties() throws ServiceException,
+            TeamspaceAlreadyExistsException, CouldNotCreateTeamspaceException,
+            CouldNotRemoveTeamspaceException {
+
         TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
         UserAdmin userAdmin = lookupUserAdmin();
-        
-        String userId = "mindquarry-user";
-        UserRO creator = userAdmin.createUser(userId, "aSecretPassword", 
-                "Mindquarry User", "surname", "an email", "the skills");
-        
-        String teamspaceId = "mindquarry-teamspace";
-        TeamspaceDefinition teamspace = teamsAdmin.createTeamspace(teamspaceId, 
-                "Mindquarry Teamspace", "a greate description", creator);
-        
-        String propKey = "workspaceUri";
-        String propValue = "/tmp/repos/mindquarry";
-        
-        teamspace.setProperty(propKey, propValue);
-        teamsAdmin.updateTeamspace(teamspace);
-        
-        TeamspaceDefinition updatedTeamspace = 
-            teamsAdmin.teamspaceDefinitionForId(teamspaceId);
-        
-        assertEquals(propValue, updatedTeamspace.getProperty(propKey));
-                
-        teamsAdmin.removeTeamspace(teamspaceId);
-    }
-    
-    public void testTeamspaceRegistry() 
-        throws ServiceException, TeamspaceAlreadyExistsException {
-        
-        TestTeamspaceListener testListener = new TestTeamspaceListener();
-        lookupTeamspaceListenerRegistry().addListener(testListener);
-        
-        TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
-        UserAdmin userAdmin = lookupUserAdmin();
-        
-        String userId = "mindquarry-user";
-        UserRO creator = userAdmin.createUser(userId, "aSecretPassword", 
-                "Mindquarry User", "surname", "an email", "the skills");
-        
-        String teamspaceId = "mindquarry-teamspace";
-        teamsAdmin.createTeamspace(teamspaceId, 
-                "Mindquarry Teamspace", "a greate description", creator);
-        
-        assertTrue(testListener.wasCalled);
-                
-        teamsAdmin.removeTeamspace(teamspaceId);
-    }
-    
-    public void testAddUserToTeamspace() 
-        throws ServiceException, TeamspaceAlreadyExistsException {
-        // please note, an admin users is created within the initialize method
-        
-        TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
-        UserAdmin userAdmin = lookupUserAdmin();
-        
+
         String userId = "mindquarry-user";
         UserRO creator = userAdmin.createUser(userId, "aSecretPassword",
                 "Mindquarry User", "surname", "an email", "the skills");
-        
-        userAdmin.createUser("newUser", "aSecretPassword", 
-                "Mindquarry User", "surname", "an email", "the skills");
-        
+
         String teamspaceId = "mindquarry-teamspace";
-        TeamspaceRO teamspace = teamsAdmin.createTeamspace(teamspaceId, 
+        TeamspaceDefinition teamspace = teamsAdmin.createTeamspace(teamspaceId,
                 "Mindquarry Teamspace", "a greate description", creator);
-        
+
+        String propKey = "workspaceUri";
+        String propValue = "/tmp/repos/mindquarry";
+
+        teamspace.setProperty(propKey, propValue);
+        teamsAdmin.updateTeamspace(teamspace);
+
+        TeamspaceDefinition updatedTeamspace = teamsAdmin
+                .teamspaceDefinitionForId(teamspaceId);
+
+        assertEquals(propValue, updatedTeamspace.getProperty(propKey));
+
+        teamsAdmin.removeTeamspace(teamspaceId);
+    }
+
+    public void testTeamspaceRegistry() throws ServiceException,
+            TeamspaceAlreadyExistsException, CouldNotCreateTeamspaceException,
+            CouldNotRemoveTeamspaceException {
+
+        TestTeamspaceListener testListener = new TestTeamspaceListener();
+        lookupTeamspaceListenerRegistry().addListener(testListener);
+
+        TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
+        UserAdmin userAdmin = lookupUserAdmin();
+
+        String userId = "mindquarry-user";
+        UserRO creator = userAdmin.createUser(userId, "aSecretPassword",
+                "Mindquarry User", "surname", "an email", "the skills");
+
+        String teamspaceId = "mindquarry-teamspace";
+        teamsAdmin.createTeamspace(teamspaceId, "Mindquarry Teamspace",
+                "a greate description", creator);
+
+        assertTrue(testListener.wasCalled);
+
+        teamsAdmin.removeTeamspace(teamspaceId);
+    }
+
+    public void testAddUserToTeamspace() throws ServiceException,
+            TeamspaceAlreadyExistsException, CouldNotCreateTeamspaceException {
+        // please note, an admin users is created within the initialize method
+
+        TeamspaceAdmin teamsAdmin = lookupTeamspaceAdmin();
+        UserAdmin userAdmin = lookupUserAdmin();
+
+        String userId = "mindquarry-user";
+        UserRO creator = userAdmin.createUser(userId, "aSecretPassword",
+                "Mindquarry User", "surname", "an email", "the skills");
+
+        userAdmin.createUser("newUser", "aSecretPassword", "Mindquarry User",
+                "surname", "an email", "the skills");
+
+        String teamspaceId = "mindquarry-teamspace";
+        TeamspaceRO teamspace = teamsAdmin.createTeamspace(teamspaceId,
+                "Mindquarry Teamspace", "a greate description", creator);
+
         Membership membership = teamsAdmin.membership(teamspace);
         assertEquals(1, membership.getMembers().size());
         assertEquals(2, membership.getNonMembers().size());
-        
-        
+
         membership.addMember(membership.getNonMembers().get(0));
         teamsAdmin.updateMembership(membership);
-        
-        
+
         Membership updatedMembership = teamsAdmin.membership(teamspace);
         assertEquals(2, updatedMembership.getMembers().size());
         assertEquals(1, updatedMembership.getNonMembers().size());
-        
+
         UserRO memberToRemove = updatedMembership.getMembers().get(0);
         updatedMembership.removeMember(memberToRemove);
         teamsAdmin.updateMembership(updatedMembership);
-        
+
         Membership originalMembership = teamsAdmin.membership(teamspace);
         assertEquals(1, originalMembership.getMembers().size());
         assertEquals(2, updatedMembership.getNonMembers().size());
     }
-    
-    private TeamspaceListenerRegistry lookupTeamspaceListenerRegistry() 
-        throws ServiceException {
-        
+
+    private TeamspaceListenerRegistry lookupTeamspaceListenerRegistry()
+            throws ServiceException {
+
         String name = DefaultListenerRegistry.class.getName();
-        
+
         return (TeamspaceListenerRegistry) lookup(name);
     }
-    
+
     static class TestTeamspaceListener implements TeamspaceListener {
-        
+
         boolean wasCalled = false;
-        
+
         public void afterTeamspaceRemoved(TeamspaceDefinition teamspace) {
             // TODO Auto-generated method stub
-            
+
         }
 
         public void beforeTeamspaceCreated(TeamspaceDefinition teamspace) {
             wasCalled = true;
         }
-        
+
     }
 }
