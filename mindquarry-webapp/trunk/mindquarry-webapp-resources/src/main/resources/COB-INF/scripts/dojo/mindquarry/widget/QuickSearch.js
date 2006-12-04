74bb4a75df9c169c35e76c30ac08c8e0b6912013
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-04 18:00:23 +0000

    adapted quick search widget according to fixed Mindquarry Search JSON response

--- mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/QuickSearch.js
+++ mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/QuickSearch.js
@@ -1,8 +1,7 @@
- /*
-    QuickSearch - extends HtmlWidget.
-    Provides a search field with popup results.
+/*
+ * QuickSearch - extends HtmlWidget.
+ * Provides a search field with popup results.
  */
- 
 dojo.provide("mindquarry.widget.QuickSearch");
 dojo.require("dojo.widget.HtmlWidget");
 
@@ -10,20 +9,20 @@ dojo.widget.defineWidget(
 	"mindquarry.widget.QuickSearch",
 	dojo.widget.HtmlWidget,
 	{
-
 		widgetType: "QuickSearch",
 		isContainer: false,
 		templateString: '<div class="mindquarry-quicksearch" style="position:relative;">' +
-											'<div class="search" style="position:relative;">' +
-												'<form dojoAttachPoint="formNode">' +
-													'<input name="q" size="20" dojoAttachPoint="inputNode" />' +
-													'<input type="button" dojoOnClick="buttonClick;" value="GO" />' +
-												'</form>' +
-											'</div>' +
-											'<div style="position:relative;">' +
-												'<div class="results" dojoAttachPoint="resultNode" style="background:white;padding:2px;font:10px sans-serif;border:1px solid black;position:absolute;top:0;left:0"></div>' + 
-											'</div>' +
-										'</div>',
+						    '<div class="search" style="position:relative;">' +
+							    '<form dojoAttachPoint="formNode">' +
+									'<input name="q" size="20" dojoAttachPoint="inputNode" />' +
+									'<input name="wt" size="20" value="mq" type="hidden"/>' +
+									'<input type="button" dojoOnClick="buttonClick;" value="GO" />' +
+								'</form>' +
+							'</div>' +
+							'<div style="position:relative;">' +
+							    '<div class="results" dojoAttachPoint="resultNode" style="background:white;padding:2px;font:10px sans-serif;border:1px solid black;position:absolute;top:0;left:0"></div>' + 
+							'</div>' +
+						'</div>',
 		
 		// template nodes
 		inputNode: null,   // the search input field
@@ -48,6 +47,7 @@ dojo.widget.defineWidget(
 		// search button click handler
 		buttonClick: function(evt) {
 			dojo.debug("QuickSearch - starting search");
+			
 			if (this._busy) return; // only one request at a time
 			this._busy = true;
 			if (this.resultNode) {
@@ -55,47 +55,53 @@ dojo.widget.defineWidget(
 				dojo.dom.removeChildren(this.resultNode); // clear any content from the resultsNode
 			}
 			var self = this;
+			
 			dojo.io.bind({
-					url: this.url,
-					mimetype: "text/json",
-					formNode: this.formNode,
-					method: "post",
-					handle: function(type, data, evt) {
-						if (type == "load") {
-								if (!data) return;
-								self.update(data);
-								self._busy = false;
-						} else if (type == "error") {
-								dojo.debug("QuickSearch - status request failed");
-						}           
-					}
+				url: this.url,
+				mimetype: "text/json",
+				formNode: this.formNode,
+				method: "get",
+				handle: function(type, data, evt) {
+					if (type == "load") {
+						if (!data) return;
+							
+						self.update(data);
+						self._busy = false;
+					} else if (type == "error") {
+						dojo.debug("QuickSearch - status request failed");
+					}           
+				}
 			});
-		
-		
 		},
 		
 		// update the results display
 		update: function(data) {
-			dojo.debug("QuickSearch- got result: " + data);
+			dojo.debug("QuickSearch - got result: " + data);
 			dojo.html.show(this.resultNode);
+			
 			var status = document.createElement("div");
 			status.appendChild(document.createTextNode("Results: " + data.response.numFound))
+			
 			this.resultNode.appendChild(status);
 			if (data.response.numFound > 0) {
 				var results = document.createElement("table");
 				var resultsbody = document.createElement("tbody");
 				results.appendChild(resultsbody);
-				for (var type in data.docs) {
-					var typerow = document.createElement("tr");
+								
+				for (var type in data.response.docs) {
+				    var typerow = document.createElement("tr");
 					typerow.setAttribute("valign", "top")
+					
 					var typecell = document.createElement("td");
 					typecell.appendChild(document.createTextNode(type));
+					
 					var rescell = document.createElement("td");
-					for (var hit in data.docs[type]) {
+					for (var hit in data.response.docs[type]) {
 						var a = document.createElement("a");
+						a.setAttribute("href", data.response.docs[type][hit].location);
+						a.appendChild(document.createTextNode(data.response.docs[type][hit].title));
 						var br = document.createElement("br");
-						a.setAttribute("href", data.docs[type][hit].url);
-						a.appendChild(document.createTextNode(data.docs[type][hit].title));
+						
 						rescell.appendChild(a);
 						rescell.appendChild(br);
 					}
