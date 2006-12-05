f564b62c7ba941057e501faf5c75f8dfef7e11a4
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-05 17:12:39 +0000

    added unique name generation for tasks (task + id) integrated redirect transformer renamed task-view-or-edit.xsl to simpler task2html.xsl

--- trunk/mindquarry-tasks/trunk/mindquarry-tasks-block/src/main/resources/COB-INF/flows/uniqueName.js
+++ trunk/mindquarry-tasks/trunk/mindquarry-tasks-block/src/main/resources/COB-INF/flows/uniqueName.js
@@ -1,5 +1,12 @@
-function createUniqueName() {
-    return "tasks";
+function createUniqueName(baseURI) {
+    var uniqueIDGenerator;
+	try {
+		uniqueIDGenerator = cocoon.getComponent("com.mindquarry.jcr.id.JCRUniqueIDGenerator");
+		return "task" + uniqueIDGenerator.getNextID(baseURI);
+	} finally {
+		cocoon.releaseComponent(uniqueIDGenerator);
+	}
+    return "task_error";
 }
 
-createUniqueName();
+createUniqueName(baseURI_);
