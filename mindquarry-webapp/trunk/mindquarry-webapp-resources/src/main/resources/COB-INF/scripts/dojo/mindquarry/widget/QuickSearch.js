b2c71b7996cd6d4c235da77b3cdc5badf77323a4
jeremy <jeremy@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-07 14:21:16 +0000

    reworked the QuickSearch Widget.

--- mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/QuickSearch.js
+++ mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/QuickSearch.js
@@ -12,134 +12,127 @@ dojo.widget.defineWidget(
 	{
 		widgetType: "QuickSearch",
 		isContainer: false,
-		templateString: '<div id="quicksearch" class="mindquarry-quicksearch" style="position:relative;">' +
-						    '<div class="search" style="position:relative;">' +
-							    '<form dojoAttachPoint="formNode">' +
-									'<input name="q" size="30" dojoAttachPoint="inputNode" />' +
-									'<input name="wt" value="mq" type="hidden"/>' +
-									'<input name="fl" value="score" type="hidden"/>' +
-									'<input type="button" dojoOnClick="buttonClick;" value="Search" />' +
-								'</form>' +
-							'</div>' +
-							'<div style="position:relative;">' +
-							    '<div class="results" dojoAttachPoint="resultNode" style="filter:alpha(opacity=60);opacity:.6;background:#fbf4d4;padding:2px;font:11px sans-serif;border:1px solid grey;position:absolute;top:0;left:0"></div>' + 
-							'</div>' +
-						'</div>',
+		
+		// config attributes, the text could be supplied via i18n on the tag
+		url: "", // the url to access to perform searches (required, set via this attr on the tag)
+		maxheight: 300, // the maximum height of the results popup
+		size: "20", // the size of the search input field
+		searchButton: "Search", // text of the search button
+		searchingStatus: "Searching ...",	// shown while searching
+		noResultsStatus: "Search finished without any results.", // shown if there are no results
+		gotResultsStatus: "Search finished with %{count} results :", // shown if there are results
+
+		templateString: '<div style="position:relative;" class="mindquarry-quicksearch">' + 
+			'<div class="search-field">' +
+				'<form dojoAttachPoint="formNode">' +
+					'<span dojoAttachPoint="widthNode">' + 
+						'<input name="q" size="${this.size}"/>' +
+						'<input name="wt" value="mq" type="hidden"/>' +
+						'<input name="fl" value="score" type="hidden"/>' +
+						'<input type="button" dojoOnClick="searchClick;" value="${this.searchButton}"/>' + 
+					'</span>' +
+				'</form>' +
+			'</div>' +
+			'<div style="position:relative;">' +
+					'<div class="result-popup" dojoAttachPoint="popupNode" style="position:absolute;top:0;left:0;overflow:auto;padding:5px;">' +
+						'<a dojoOnClick="closeClick;" href="#" style="background:url(\'../buttons/close.png\');display:block;height:10px;width:10px;float:right;"></a>' 	+
+						'<span class="result-status" dojoAttachPoint="resultStatus" style="margin-bottom:8px;"></span>' +
+						'<table width="100%" class="result-table"><tbody dojoAttachPoint="resultNode"></tbody></table>' 	+
+					'</div>' + 
+			'</div>' +
+		'</div>',
+		
+		typeTemplate: '<tr valign="top">' +
+										'<td width="25%" align="right" class="task-column">%{type}</td>' + 
+										'<td class="file-column"><table width="100%"><tbody>%{hits}</tbody></table></td>' + 
+									'</tr>',
+									
+		hitTemplate: '<tr valign="top"><td width="32"><div style="width:30px;height:12px;border:1px solid #666;position:relative;">' + 
+									 '<div style="background:#ddf;height:10px;width:%{score}%;margin:1px 0;padding:0;"> </div>' + 
+									 '<div style="position:absolute;top:0;right:0;margin:0;padding:0;color:#444;font-size:10px;">%{score}</div>' + 
+								 '</div></td>' +
+								'<td><a href="%{uri}">%{title}</a></td></tr>',
 		
 		// template nodes
-		inputNode: null,   // the search input field
-		formNode: null,    // the search form
-		resultNode: null,  // the results container
-		
-		// config attributes
-		url: "", // the url to access to perform searches (set via this attr on the tag)
-		height: "",
-		width: "",
+		formNode: null,     // the search form
+		popupNode: null,    // the results popup container
+		resultNode: null,   // the results container
+		resultStatus: null, // the results status node
 		
 		// instance properties
 		_busy: false,
 
 		// widget interface
 		postCreate: function(){
-			dojo.html.hide(this.resultNode);
-			this.resultNode.style.width = this.width + "px";
-			this.resultNode.style.height = this.height + "px";
+			dojo.html.hide(this.popupNode);
+			this.popupNode.style.width = dojo.style.getOuterWidth(this.widthNode) + "px";
 		},
 		
 		// search button click handler
-		buttonClick: function(evt) {
+		searchClick: function(evt) {
 			dojo.debug("QuickSearch - starting search");
 			
 			if (this._busy) return; // only one request at a time
 			this._busy = true;
-			if (this.resultNode) {
-				dojo.html.hide(this.resultNode);
-				dojo.dom.removeChildren(this.resultNode); // clear any content from the resultsNode
-			}
-			var self = this;
+			this._setStatus(this.searchingStatus);
+			dojo.dom.removeChildren(this.resultNode); // clear any rows from the resultsNode
+			dojo.html.show(this.popupNode);
 			
 			dojo.io.bind({
 				url: this.url,
 				mimetype: "text/json",
 				formNode: this.formNode,
 				method: "get",
-				handle: function(type, data, evt) {
+				handle: dojo.lang.hitch(this, function(type, data, evt) {
 					if (type == "load") {
 						if (!data) return;
-							
-						self.update(data);
-						self._busy = false;
+						this.update(data);
+						this._busy = false;
 					} else if (type == "error") {
 						dojo.debug("QuickSearch - status request failed");
 					}           
-				}
+				})
 			});
+		},
+
+		// handle clicks on the close button
+		closeClick: function(evt) {
+		  dojo.html.hide(this.popupNode);
+    },
+		
+		// set the status area
+		_setStatus: function(status) {
+			this.resultStatus.innerHTML = status;
+		},
+		
+		// use a pair of mini templates to build the results
+		_buildResults: function(docs) {
+			var types = [];
+			for (var type in docs) {
+				var hits = [];
+				for (var hit in docs[type]) {
+					hits.push(dojo.string.substituteParams(this.hitTemplate, 
+						{uri: docs[type][hit].location, title: docs[type][hit].title, score: docs[type][hit].score}
+					));
+				}
+				types.push(dojo.string.substituteParams(this.typeTemplate, {type: type, hits: hits.join("")})); 
+			}
+			return types.join("");
 		},
 		
 		// update the results display
 		update: function(data) {
-			dojo.debug("QuickSearch - got result: " + data);
-			dojo.html.show(this.resultNode);
-			
-			// create close button
-			var closeButton = document.createElement("a");
-			closeButton.appendChild(document.createTextNode(""))
-			closeButton.setAttribute("href", "#");
-			closeButton.style.background = "url('../buttons/close.png')";
-			closeButton.style.display = "block";
-			closeButton.style.height = "10px";
-			closeButton.style.width = "10px";
-			closeButton.style.cssFloat = "right";
-			dojo.event.connect(closeButton, "onclick", this, "closeResults");
-			
-			this.resultNode.appendChild(closeButton);
-			
-			// create status info
-			var status = document.createElement("div");
+			dojo.debug("QuickSearch - got results: " + data.response.numFound);
 			if (data.response.numFound > 0) {
-			    status.appendChild(document.createTextNode("Search finished with " + data.response.numFound + " results."));
-			} else {
-			    status.appendChild(document.createTextNode("Search finished without any results."));
-			}
-			status.style.marginBottom = "8px";
-			this.resultNode.appendChild(status);
-			
-			if (data.response.numFound > 0) {
-				var results = document.createElement("table");
-				var resultsbody = document.createElement("tbody");
-				results.appendChild(resultsbody);
-								
-				for (var type in data.response.docs) {
-				    var typerow = document.createElement("tr");
-					typerow.setAttribute("valign", "top");
-										
-					var typecell = document.createElement("td");
-					typecell.appendChild(document.createTextNode(type + ":"));
-					
-					var rescell = document.createElement("td");
-					for (var hit in data.response.docs[type]) {
-						var a = document.createElement("a");
-						a.setAttribute("href", data.response.docs[type][hit].location);
-						a.appendChild(document.createTextNode(data.response.docs[type][hit].title));
-						rescell.appendChild(a);
-						
-						var score = document.createElement("span");
-						score.appendChild(document.createTextNode(" (Score: " + data.response.docs[type][hit].score + ")"));
-						rescell.appendChild(score);
-						
-						var br = document.createElement("br");
-						rescell.appendChild(br);
-					}
-					typerow.appendChild(typecell);
-					typerow.appendChild(rescell);
-					resultsbody.appendChild(typerow);
+			  this._setStatus(dojo.string.substituteParams(this.gotResultsStatus, {count:data.response.numFound}));
+				this.resultNode.innerHTML = this._buildResults(data.response.docs);
+				if (dojo.style.getOuterHeight(this.resultNode) > this.maxheight) {
+					this.popupNode.style.height = this.maxheight + "px"; 					
 				}
-				this.resultNode.appendChild(results);
+			} else {
+				this._setStatus(this.noResultsStatus);
 			}
-		},
+		}
 		
-		closeResults: function(evt) {
-		    dojo.html.hide(this.resultNode);
-        }
 	}
 );
