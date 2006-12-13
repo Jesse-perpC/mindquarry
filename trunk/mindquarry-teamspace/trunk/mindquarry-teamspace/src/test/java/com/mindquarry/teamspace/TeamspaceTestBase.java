7797a163d042cdbd2de2302968b30511533f3aed
bastian <bastian@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-13 09:44:23 +0000

    * made IndexClient avalon component to spring component * added heavy load tests to surefire exclude lists

--- trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/test/java/com/mindquarry/teamspace/TeamspaceTestBase.java
+++ trunk/mindquarry-teamspace/trunk/mindquarry-teamspace/src/test/java/com/mindquarry/teamspace/TeamspaceTestBase.java
@@ -5,21 +5,13 @@ package com.mindquarry.teamspace;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.avalon.framework.service.ServiceException;
 import org.apache.excalibur.source.Source;
 import org.apache.excalibur.source.SourceResolver;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 import com.mindquarry.jcr.jackrabbit.JCRTestBase;
-import com.mindquarry.teamspace.TeamspaceAdmin;
 import com.mindquarry.user.UserAdmin;
 
 
@@ -29,42 +21,10 @@ import com.mindquarry.user.UserAdmin;
  */
 public abstract class TeamspaceTestBase extends JCRTestBase {
     
-
-    // add spring bean definitions to component configuration settings
-    protected void addSettings(BeanDefinitionRegistry registry) {
-                
-        GenericApplicationContext ctx = new GenericApplicationContext();
-        XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(ctx);
-        
-        Resource[] springConfigResources = findSpringConfigResources();
-        
-        for (Resource resource : springConfigResources)
-            xmlReader.loadBeanDefinitions(resource);
-        
-        
-        for (String beanName : ctx.getBeanDefinitionNames()) {
-            BeanDefinition beanDefinition = ctx.getBeanDefinition(beanName);
-            registry.registerBeanDefinition(beanName, beanDefinition);
-            
-            for (String alias : ctx.getAliases(beanName)) 
-                registry.registerAlias(beanName, alias);
-        }
-
-        super.addSettings(registry);
-    }
-
-    private Resource[] findSpringConfigResources() {
-        
-        List<Resource> resultList = new LinkedList<Resource>();
-        
-        String[] resourceNames = new String[] {
-                "META-INF/cocoon/spring/teamspace-context.xml"
-        };
-        
-        for (String resourceName : resourceNames)
-            resultList.add(new ClassPathResource(resourceName));
-        
-        return resultList.toArray(new Resource[resultList.size()]);
+    protected List<String> springConfigClasspathResources() {
+        List<String> result = super.springConfigClasspathResources();
+        result.add("META-INF/cocoon/spring/teamspace-context.xml");
+        return result;
     }
     
     protected Source resolveSource(String uri) throws ServiceException, IOException {
