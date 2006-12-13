2dd91729215fb4857340bfcb80f718fa72a7b5d5
bastian <bastian@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-13 10:25:04 +0000

    added check if entities, that should be created, do already exist

--- mindquarry-persistence/trunk/castor-persistence/src/test/java/com/mindquarry/persistence/castor/CastorPersistenceTest.java
+++ mindquarry-persistence/trunk/castor-persistence/src/test/java/com/mindquarry/persistence/castor/CastorPersistenceTest.java
@@ -7,6 +7,7 @@ import javax.transaction.SystemException;
 
 import org.apache.avalon.framework.service.ServiceException;
 
+import com.mindquarry.common.persistence.PersistenceException;
 import com.mindquarry.common.persistence.Session;
 import com.mindquarry.common.persistence.SessionFactory;
 
@@ -17,18 +18,17 @@ public class CastorPersistenceTest extends CastorPersistenceTestBase {
         SessionFactory sessionFactory = (SessionFactory) lookup(SessionFactory.ROLE);
         Session session = sessionFactory.currentSession();
         
-        UserEntity user = (UserEntity) session.newEntity(UserEntity.class);
-        
-        user.setId("bastian");
-        
+        UserEntity user = new UserEntity();        
+        user.setId("bastian");        
         user.setName("Bastain");
         user.setSurname("Steinert");
+        session.persist(user);
         
-        TeamspaceEntity teamspace = 
-            (TeamspaceEntity) session.newEntity(TeamspaceEntity.class);
+        TeamspaceEntity teamspace = new TeamspaceEntity();
         teamspace.setDescription("a great description");
         teamspace.setId("mindquarry");
         teamspace.setName("Mindquarry");
+        session.persist(teamspace);
         
         session.commit();
         
@@ -50,5 +50,40 @@ public class CastorPersistenceTest extends CastorPersistenceTestBase {
         assertTrue(queryResult.isEmpty());       
         
         session.commit();
+    }
+    
+    public void testDuplicateIds() throws ServiceException {
+        SessionFactory sessionFactory = (SessionFactory) lookup(SessionFactory.ROLE);
+        
+        
+        UserEntity user = new UserEntity();        
+        user.setId("bastian");        
+        user.setName("Bastain");
+        user.setSurname("Steinert");
+        
+        Session session = sessionFactory.currentSession();
+        session.persist(user);        
+        session.commit();
+        
+        
+        UserEntity duplicateUser = new UserEntity();        
+        duplicateUser.setId("bastian");        
+        duplicateUser.setName("Bastain");
+        duplicateUser.setSurname("Steinert");
+        
+        boolean recognizedDuplicateIds = false;
+        session = sessionFactory.currentSession();
+        try {
+            session.persist(duplicateUser);
+            session.commit();
+        } catch (PersistenceException e) {
+            recognizedDuplicateIds = true;
+        }
+        
+        session = sessionFactory.currentSession();        
+        session.delete(user);
+        
+        if (! recognizedDuplicateIds)
+            fail("did not recognize duplicate ids");
     }
 }
