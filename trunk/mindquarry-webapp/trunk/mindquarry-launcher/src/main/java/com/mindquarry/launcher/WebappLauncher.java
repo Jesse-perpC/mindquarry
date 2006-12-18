ff7c8aa7dc6068c75125e6fb9f843a3b532cffd5
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-18 17:35:41 +0000

    added JSP dependencies

--- trunk/mindquarry-webapp/trunk/mindquarry-launcher/src/main/java/com/mindquarry/launcher/WebappLauncher.java
+++ trunk/mindquarry-webapp/trunk/mindquarry-launcher/src/main/java/com/mindquarry/launcher/WebappLauncher.java
@@ -5,17 +5,7 @@ package com.mindquarry.launcher;
 
 import java.io.File;
 
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Handler;
-import org.mortbay.jetty.NCSARequestLog;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.handler.ContextHandlerCollection;
-import org.mortbay.jetty.handler.DefaultHandler;
-import org.mortbay.jetty.handler.HandlerCollection;
-import org.mortbay.jetty.handler.RequestLogHandler;
-import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.webapp.WebAppContext;
-import org.mortbay.thread.BoundedThreadPool;
 import org.mortbay.xml.XmlConfiguration;
 
 /**
@@ -26,10 +16,11 @@ import org.mortbay.xml.XmlConfiguration;
  */
 public class WebappLauncher {
     public static void main(String[] args) throws Exception {
-    	Server server = new Server();
-    	XmlConfiguration configuration = new XmlConfiguration(new File("mindquarry.xml").toURL()); //or use new XmlConfiguration(new FileInputStream("myJetty.xml"));
-    	configuration.configure(server);
-    	server.start();
+        Server server = new Server();
+        XmlConfiguration configuration = new XmlConfiguration(new File(
+                "mindquarry.xml").toURL()); //$NON-NLS-1$
+        configuration.configure(server);
+        server.start();
 
         System.out.println("Jetty started."); //$NON-NLS-1$
     }
