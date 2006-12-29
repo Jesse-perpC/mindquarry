1927cf13cf1589dbe812e8a72f99fb5545627aef
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2006-12-29 14:36:22 +0000

    [maven-scm] copy for tag mindquarry-teamspace-block-0.0.1

---
+++ mindquarry-teamspace/tags/mindquarry-teamspace-block/mindquarry-teamspace-block-0.0.1/src/main/resources/COB-INF/flows/users.js
@@ -0,0 +1,17 @@
+
+function userById(userId) {
+	var userAdmin = cocoon.getComponent(UserAdmin.ROLE);	
+	var result = userAdmin.userById(userId);	
+	cocoon.releaseComponent(userAdmin);
+	return result;
+}
+ 
+function existsUserWithId(userIdWidget) {
+	var userModel = model_.userModel;
+	var userExists = (null != userById(userModel.userId));
+	if (userExists) {
+		userIdWidget.setValidationError(
+			new Packages.org.apache.cocoon.forms.validation.ValidationError("userId already exists!"));
+	}
+	return false;
+}
