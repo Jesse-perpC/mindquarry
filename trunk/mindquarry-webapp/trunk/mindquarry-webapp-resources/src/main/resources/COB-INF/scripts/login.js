7634869ed20181200d81e9d7949364d98d658104
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-01-16 11:05:38 +0000

    - added license statements - fixed lightbox processing for URL that already contain some parameters -

--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/login.js
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/login.js
@@ -1,7 +1,16 @@
-/**
- * Copyright (C) 2006 Mindquarry GmbH, All Rights Reserved
- */
- 
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */ 
 dojo.addOnLoad(function()
 {	
 	var index = -1;
@@ -125,6 +134,7 @@ function createLogoutLink(httplogout, pathToWebappRoot)
     link.href = "#";
     link.onclick = function() {logout(pathToWebappRoot.href);};
     link.id = "http-logout-link";
+    link.title = "Log out";
     link.innerHTML = "Log out";
     
     httplogout.innerHTML = "";
