02374dc375971ab31dbb728d807a69c71c7d6614
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-01-16 12:23:33 +0000

    create a user info listing at /userinfo

--- trunk/mindquarry-teamspace/trunk/mindquarry-teamspace-block/src/main/resources/COB-INF/flows/teamspaces-view.js
+++ trunk/mindquarry-teamspace/trunk/mindquarry-teamspace-block/src/main/resources/COB-INF/flows/teamspaces-view.js
@@ -1,9 +1,20 @@
 /*
- * Copyright (C) 2006 Mindquarry GmbH, All Rights Reserved
- */
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */ 
  
 importClass(Packages.com.mindquarry.teamspace.TeamspaceQuery);
 importPackage(Packages.com.mindquarry.user);
+cocoon.load("flows/users.js");
 
 function listTeamspacesForUser() {
     var teamspaceQuery = cocoon.getComponent(TeamspaceQuery.ROLE);
@@ -11,7 +22,7 @@ function listTeamspacesForUser() {
     var userId = cocoon.parameters["username"];
     var teamspaces = teamspaceQuery.teamspacesForUser(userId);
     
-    var parameterMap = { "teamspaces" : teamspaces }
+    var parameterMap = { "teamspaces" : teamspaces, "userId" : userById(userId) }
     var target = cocoon.parameters["target"];
     cocoon.sendPage(target, parameterMap);
 }
--- trunk/mindquarry-teamspace/trunk/mindquarry-teamspace-block/src/main/resources/COB-INF/flows/users.js
+++ trunk/mindquarry-teamspace/trunk/mindquarry-teamspace-block/src/main/resources/COB-INF/flows/users.js
@@ -1,4 +1,16 @@
-
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */ 
 function userById(userId) {
 	var userAdmin = cocoon.getComponent(UserAdmin.ROLE);	
 	var result = userAdmin.userById(userId);	
