6ff9919031a82d3c6770f85f30d701c86d1ee1f9
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2007-01-21 12:28:22 +0000

    digest string module that turns strings into numbers

---
+++ mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/java/com/mindquarry/webapp/modules/DigestStringModule.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.webapp.modules;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.cocoon.components.modules.input.AbstractInputModule;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.web.context.support.ServletContextResourcePatternResolver;
+
+/**
+ * 
+ * @author <a href="mailto:lars.trieloff@mindquarry.com">
+ *         Lars Trieloff</a>
+ *
+ */
+public class DigestStringModule extends AbstractInputModule {
+    
+    @Override
+    public Object getAttribute(final String name, Configuration config, Map objectModel) throws ConfigurationException {
+      
+        int colonindex = name.indexOf(":");
+        
+        String before = name.substring(0, colonindex);
+        String after = name.substring(colonindex + 1);
+        
+        int hash = Math.abs(name.hashCode()) % Integer.parseInt(before);
+        
+        return "" + hash;
+    }
+
+}
