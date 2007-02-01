c019f82d9aa9d626656dffeabffbce28734ef7bc
alex <alex@b115fd6e-e050-0410-b27a-7792256590da>
2007-02-01 18:02:28 +0000

    added TimerCFormAction dojo widget (for a cform action widget) that calls itself periodically (defined via delay="ms" in html)

---
+++ mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/scripts/dojo/mindquarry/widget/TimerCFormAction.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+ 
+dojo.provide("mindquarry.widget.TimerCFormAction");
+
+dojo.require("dojo.widget.*");
+dojo.require("dojo.html");
+dojo.require("dojo.event");
+//dojo.require("dojo.lang.timing.Timer"); // 0.4 ???
+dojo.require("dojo.animation.Timer"); // 0.3.1 - seems deprecated later
+
+dojo.widget.tags.addParseTreeHandler("dojo:TimerCFormAction");
+dojo.widget.manager.registerWidgetPackage("mindquarry.widget");
+mindquarry.widget.TimerCFormAction = function() {
+	dojo.widget.DomWidget.call(this);
+	
+	var cform = null;
+}
+
+
+dojo.inherits(mindquarry.widget.TimerCFormAction, dojo.widget.DomWidget);
+
+dojo.lang.extend(mindquarry.widget.TimerCFormAction, {
+	widgetType: "TimerCFormAction",
+	isContainer: true,
+	
+	buildRendering: function(args, parserFragment, parentWidget) {
+        // Magical statement to get the dom node, stolen in DomWidget
+	    this.domNode = parserFragment["dojo:"+this.widgetType.toLowerCase()].nodeRef;
+	    this.cform = parentWidget;
+	    
+		var delay = dojo.html.getAttribute(this.domNode, "delay");
+		
+		if (delay == null) {
+			delay = 60000; // 1 minute
+		}
+		
+	    // call a keep-alive on each delay
+	    var timer = new dojo.animation.Timer(delay);
+	    
+	    // store in local variables for onTick function
+	    var domNode = this.domNode;
+	    var cform = this.cform;
+	    
+	    timer.onTick = function() {
+	        if (cform==null) {
+	        	var form = cocoon.forms.getForm(domNode);
+	        	var dojoId = form.getAttribute("dojoWidgetId");
+	        	if (dojoId) {
+	        		cform = dojo.widget.byId(dojoId);
+	        	}
+	        }
+        	cform.submit(domNode.name);	    	
+	    }
+	    
+	    timer.start();
+    }	
+});
