e83c03a172eac8cdbf89dae1203fd71ae36f0a9d
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-02-05 16:43:37 +0000

    unsigned and re-signed jars

--- trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/client/sign-jars.sh
+++ trunk/mindquarry-webapp/trunk/mindquarry-webapp-resources/src/main/resources/COB-INF/client/sign-jars.sh
@@ -3,5 +3,7 @@
 KEYSTORE="$1"
 
 for file in $( find . -name "*.jar" ); do
-	jarsigner -keystore $KEYSTORE -storepass mindquarry $file mindquarry 
+	#sh unsigner.sh $file
+	jarsigner -keystore $KEYSTORE -storepass mindquarry $file mindquarry
+	#jarsigner -verify -verbose -certs -keystore $KEYSTORE -storepass mindquarry $file mindquarry 
 done
