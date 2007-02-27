bdb514e90576c41d27e55e22334aa3f11d97b777
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-02-27 16:35:31 +0000

    fixed MPL statements

--- trunk/mindquarry-jcr/trunk/mindquarry-jcr-source/src/test/java/com/mindquarry/jcr/xml/source/JCRSourceComplexXmlTest.java
+++ trunk/mindquarry-jcr/trunk/mindquarry-jcr-source/src/test/java/com/mindquarry/jcr/xml/source/JCRSourceComplexXmlTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2007 MindQuarry GmbH, All Rights Reserved
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
  * 
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
