1c594b8a4437d6f1bfaa7ed23609b0dc944e701c
bastian <bastian@b115fd6e-e050-0410-b27a-7792256590da>
2007-03-08 17:56:14 +0000

    started jcr/java mapping project

---
+++ mindquarry-persistence/trunk/jcr-persistence/src/main/java/com/mindquarry/persistence/jcr/mapping/model/ModelManager.java
@@ -0,0 +1,11 @@
+package com.mindquarry.persistence.jcr.mapping.model;
+
+
+public class ModelManager  {
+
+    public void configure() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ mindquarry-persistence/trunk/jcr-persistence/src/test/java/com/mindquarry/persistence/jcr/User.java
@@ -0,0 +1,46 @@
+package com.mindquarry.persistence.jcr;
+
+
+public class User  {
+    
+    private String login;
+    private String pwd;
+    private String firstname;
+    private String lastname;
+    
+    public User() {
+        
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
