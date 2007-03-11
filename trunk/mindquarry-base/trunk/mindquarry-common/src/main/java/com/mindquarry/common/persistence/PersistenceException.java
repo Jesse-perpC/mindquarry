f42b269fd41e474283bfb0a941f2cc9840652e96
bastian <bastian@b115fd6e-e050-0410-b27a-7792256590da>
2007-03-11 20:16:45 +0000

    * added some helper classes to enable fp like work with lists * added StringUtil class to support common tasks (e.g. efficient concat of multiple string objects)

--- trunk/mindquarry-base/trunk/mindquarry-common/src/main/java/com/mindquarry/common/persistence/PersistenceException.java
+++ trunk/mindquarry-base/trunk/mindquarry-common/src/main/java/com/mindquarry/common/persistence/PersistenceException.java
@@ -27,6 +27,10 @@ public class PersistenceException extends RuntimeException {
 	public PersistenceException(String message) {
 		super(message);
 	}
+    
+    public PersistenceException(Throwable cause) {
+        super(cause);
+    }
 	
 	public PersistenceException(String message, Throwable cause) {
 		super(message, cause);
