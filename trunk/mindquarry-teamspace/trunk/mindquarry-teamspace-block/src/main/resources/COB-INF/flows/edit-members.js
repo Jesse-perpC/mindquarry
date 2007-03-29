3db02cc857315f305821f7867814308d7ceb968b
jan <jan@b115fd6e-e050-0410-b27a-7792256590da>
2007-03-29 14:36:36 +0000

    teamspace.css: added .teams prefix to every rule teamspaces.js: replaced cocoon.redirectTo  to cocoon.redirectTo('cocoon:/redirectTo/') use cocoon pipeline for redirect edit-members.js: replaced cocoon.redirectTo  to cocoon.redirectTo('cocoon:/redirectTo/') use cocoon pipeline for redirect edit-user.js: WORKAROUND for cocoon xml-statt-xhtml bug (lars) redirect-browserupdate.jx: Added for lightbox BU:Redirect jx-update sitemap.xmap: Added Lightbox to sitemap

--- trunk/mindquarry-teamspace/trunk/mindquarry-teamspace-block/src/main/resources/COB-INF/flows/edit-members.js
+++ trunk/mindquarry-teamspace/trunk/mindquarry-teamspace-block/src/main/resources/COB-INF/flows/edit-members.js
@@ -11,7 +11,7 @@
  * License for the specific language governing rights and limitations
  * under the License.
  */ 
-cocoon.load("resource://org/apache/cocoon/forms/flow/javascript/Form.js");
+cocoon.load("resource://com/mindquarry/cocoon/forms/Form.js");
 cocoon.load("flows/upload-photo.js");
 
 var teamspaceQuery_;
@@ -38,7 +38,7 @@ function processEditMembersForm(form) {
 	setCreateUserEmbeddedMode();
 	form.showForm("edit-members.instance");
 	
-	cocoon.redirectTo(redirectURL);
+	cocoon.redirectTo("cocoon:/redirectTo/" + redirectURL);
 }
 
 function loadModelWithMembership() {
@@ -71,16 +71,15 @@ function processCreateUser(form) {
 	
 	model_ = form.getModel();
 	form_ = form;
-			
+	
 	activateUserForm();
 	setCreateUserStandaloneMode();
-		
-	form.showForm("edit-members.instance");
 	
-	if (form.submitId != "cancelSubmit")
+	form.showForm("edit-members.instance");
+	if (form.submitId != "cancelSubmit") {
 		createUser();
-		
-	cocoon.redirectTo(redirectURL);
+	}
+	cocoon.redirectTo("cocoon:/redirectTo/" + redirectURL);
 }
 
 function createUser() {
