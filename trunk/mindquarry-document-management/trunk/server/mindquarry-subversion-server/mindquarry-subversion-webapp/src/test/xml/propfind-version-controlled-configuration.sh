0d2a48f7f41fcfa3090edd0e7c0249b8db979c80
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2007-04-12 07:14:15 +0000

    better naming of operations

--- trunk/mindquarry-document-management/trunk/server/mindquarry-subversion-server/mindquarry-subversion-webapp/src/test/xml/propfind-version-controlled-configuration.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -v -T propfind-version-controlled-configuration.xml -H "Content-Type: text/xml" -X PROPFIND  http://localhost:8888/repos/repo1/test.txt | xmllint --nowarning --format -
