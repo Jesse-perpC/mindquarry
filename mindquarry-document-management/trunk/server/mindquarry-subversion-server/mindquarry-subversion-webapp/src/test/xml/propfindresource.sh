cb0165dc7a25f270f67d0b6910f805694e9b0e95
lars <lars@b115fd6e-e050-0410-b27a-7792256590da>
2007-04-22 20:36:57 +0000

    propfindhandler is able to handle basic propfinds on resources

--- mindquarry-document-management/trunk/server/mindquarry-subversion-server/mindquarry-subversion-webapp/src/test/xml/propfindresource.sh
+++ mindquarry-document-management/trunk/server/mindquarry-subversion-server/mindquarry-subversion-webapp/src/test/xml/propfindresource.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -v -T propfindresource.xml -H "Content-Type: text/xml" -X PROPFIND  http://localhost:8888/repos/repo1/software/test.txt | xmllint --nowarning --format -
+curl -v -T propfindresource.xml -H "Content-Type: text/xml" -X PROPFIND  http://localhost:8889/repo/software/test.txt | xmllint --nowarning --format -
