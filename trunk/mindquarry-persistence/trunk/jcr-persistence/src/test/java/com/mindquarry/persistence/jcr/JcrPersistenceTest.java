9a4d409c11b4234dbf0541250012609e87cb2d55
bastian <bastian@b115fd6e-e050-0410-b27a-7792256590da>
2007-04-23 08:53:23 +0000

    made test moethods self contained

--- trunk/mindquarry-persistence/trunk/jcr-persistence/src/test/java/com/mindquarry/persistence/jcr/JcrPersistenceTest.java
+++ trunk/mindquarry-persistence/trunk/jcr-persistence/src/test/java/com/mindquarry/persistence/jcr/JcrPersistenceTest.java
@@ -19,6 +19,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.ValueFormatException;
@@ -35,10 +36,12 @@ import com.mindquarry.persistence.jcr.query.QueryException;
 public class JcrPersistenceTest extends JcrPersistenceTestBase {
     
     private static final String TEST_USER_ID = "testUser";
+    private static final String TEST_TEAM_ID = "jcr-persistence";
     
     private SessionFactory sessionFactory_;
     
     protected void setUp() throws Exception {
+        //System.setProperty("xenodot", "true");
         super.setUp();
         
         JavaConfiguration configuration = new JavaConfiguration();
@@ -50,6 +53,23 @@ public class JcrPersistenceTest extends JcrPersistenceTestBase {
         persistence.configure(configuration);
         
         sessionFactory_ = persistence;    
+    }
+    
+    protected void tearDown() throws Exception {
+               
+        javax.jcr.Session jcrSession = getJcrSession();
+        Node rootNode = jcrSession.getRootNode();
+        
+        String[] entityFolders = new String[] {
+                User.PARENT_FOLDER, Team.PARENT_FOLDER, Group.PARENT_FOLDER};
+        
+        for (String entityFolder : entityFolders) {
+            if (rootNode.hasNode(entityFolder))
+                rootNode.getNode(entityFolder).remove();
+        }
+        jcrSession.save();
+        
+        super.tearDown();  
     }
     
     private Session currentSession() {
@@ -66,97 +86,162 @@ public class JcrPersistenceTest extends JcrPersistenceTestBase {
         return user;
     }
     
-    /*
-    public void testPersistUser() throws Exception {
-        currentSession().persist(makeTestUser());
-        
-        String contentNodePath = concat(User.PARENT_FOLDER, "/", 
+    private String testUserContentNodePath() {
+        return concat(User.PARENT_FOLDER, "/", 
                 TEST_USER_ID, "/", "jcr:content");
-        
-        javax.jcr.Session jcrSession = getJcrSession(); 
-        Node rootNode = jcrSession.getRootNode();
-        assertTrue(rootNode.hasNode(contentNodePath));
-        
-        Node contentNode = rootNode.getNode(contentNodePath);
-        assertEquals("pwd", textValueOfChild(contentNode, "pwd"));
-        assertEquals("test", textValueOfChild(contentNode, "firstname"));
-        assertEquals("test", textValueOfChild(contentNode, "lastname"));
-        
-        rootNode.getNode(User.PARENT_FOLDER).remove();
-        jcrSession.save();
+    }
+    
+    private Team makeTestTeam() {
+        Team team = new Team();
+        team.name = TEST_TEAM_ID;
+        team.title = "Jcr Persistence title";
+        team.description = "bla blub";
+        return team;
+    }
+    
+    private String testTeamContentNodePath() {
+        return concat(Team.PARENT_FOLDER, "/", TEST_TEAM_ID, "/", 
+                TEST_TEAM_ID, ".xml", "/" , "jcr:content");
     }
     
     private String textValueOfChild(Node contentNode, String childName) 
         throws Exception {
         
-        Node textNode = contentNode.getNode(childName + "/text");
+        return textValue(contentNode.getNode(childName));
+    }
+    
+    private String textValue(Node node) throws Exception {
+        
+        Node textNode = node.getNode("text");
         return textNode.getProperty("xt:characters").getString();
     }
     
-    /*
-    public void testPersistCollectionProperties() throws Exception {        
-        User user = new User();
-        user.setLogin("testUser");
-        user.setPwd("pwd");
-        user.firstname = "test";
-        user.lastname = "test";        
+    public void testPersistUser() throws Exception {
+        User user = makeTestUser();
         currentSession().persist(user);
+         
+        Node rootNode = getJcrSession().getRootNode();
+        assertTrue(rootNode.hasNode(testUserContentNodePath()));
+        
+        Node contentNode = rootNode.getNode(testUserContentNodePath());
+        assertEquals(user.getPwd(), textValueOfChild(contentNode, "pwd"));
+        assertEquals(user.firstname, textValueOfChild(contentNode, "firstname"));
+        assertEquals(user.lastname, textValueOfChild(contentNode, "lastname"));
     }
-    */
-    public void testUpdateUser() {
-        User user = new User();
-        user.setLogin("testUser");
-        user.setPwd("pwd");
-        user.firstname = "test";
-        user.lastname = "test";        
-        currentSession().persist(user);
+    
+    public void testPersistCompositeTeamEntity() throws Exception {        
+        Team team = makeTestTeam();       
+        currentSession().persist(team);
+        
+        Node rootNode = getJcrSession().getRootNode();
+        assertTrue(rootNode.hasNode(testTeamContentNodePath()));
+        
+        Node contentNode = rootNode.getNode(testTeamContentNodePath());
+        assertEquals(team.title, textValueOfChild(contentNode, "title"));
+        assertEquals(team.description, 
+                textValueOfChild(contentNode, "description"));
+    }
+    
+    public void testPersistCollectionProperties() throws Exception {        
+        User user = makeTestUser();
         
         List<String> skills = new LinkedList<String>(); 
         skills.add("foo");
         skills.add("bar");        
-        
         user.setSkills(skills);        
-        user.setSkillsArray(skills.toArray(new String[skills.size()]));
+        user.setSkillsArray(skills.toArray(new String[skills.size()]));        
+        currentSession().persist(user);
         
-        Team team = new Team();
-        team.name = "jcr-persistence";
-        team.title = "Jcr Persistence";
-        team.description = "bla blub";
+        Node rootNode = getJcrSession().getRootNode();
+        Node contentNode = rootNode.getNode(testUserContentNodePath());
         
-        List<String> bars = new LinkedList<String>();
-        bars.add("bla");
-        bars.add("blub");        
-        team.fooMap.put("foo", bars);
-        currentSession().persist(team);
+        String[] collectionProperties = new String[] {"skills", "skillsArray"};
+        for (String collectionProperty : collectionProperties) {
+            Node collectionNode = contentNode.getNode(collectionProperty);
+            assertEquals(2, collectionNode.getNodes().getSize());
+            
+            NodeIterator itemIt = collectionNode.getNodes();
+            assertEquals("foo", textValue(itemIt.nextNode()));
+            assertEquals("bar", textValue(itemIt.nextNode()));
+        }
+    }
+
+    public void testUpdateUser() throws Exception {
+        User user = makeTestUser();     
+        currentSession().persist(user);
         
-        List<Team> teams = new LinkedList<Team>();
-        teams.add(team);
-        user.setTeams(teams);
-        
+        user.lastname = "updatedLastName";        
         currentSession().update(user);
+         
+        Node rootNode = getJcrSession().getRootNode();        
+        Node contentNode = rootNode.getNode(testUserContentNodePath());
+        assertEquals(user.lastname, textValueOfChild(contentNode, "lastname"));
+    }
+    
+    public void testQueryUsersByLogin() throws Exception {
+        currentSession().persist(makeTestUser());
+        
+        Session session = sessionFactory_.currentSession();
+        List<Object> queryResult = session.query("userByLogin", TEST_USER_ID);
+        
+        assertEquals(1, queryResult.size());
+        User user = (User) queryResult.get(0);
+        assertEquals(TEST_USER_ID, user.getLogin());
     }
     
     public void testPersistUserReferencingTeams() {
         
+        Team team = makeTestTeam();      
+        currentSession().persist(team);
+        
+        List<Team> teams = new LinkedList<Team>();
+        teams.add(team);
+        User user = makeTestUser();
+        user.setTeams(teams);
+        
+        currentSession().persist(user);
+        
+        List<Object> queryResult = 
+            currentSession().query("userByLogin", TEST_USER_ID);
+        User queriedUser = (User) queryResult.get(0);
+        
+        assertEquals(1, queriedUser.getTeams().size());
+        assertEquals(TEST_TEAM_ID, queriedUser.getTeams().get(0).name);
     }
     
     public void testQueryUsersForTeam() throws ServiceException {
         
+        Team team = makeTestTeam();      
+        currentSession().persist(team);
+        
+        List<Team> teams = new LinkedList<Team>();
+        teams.add(team);
+        User user = makeTestUser();
+        user.setTeams(teams);
+        
+        currentSession().persist(user);        
+        
         Session session = sessionFactory_.currentSession();
-        List<Object> queryResult = session.query("getUsersForTeam", "jcr-persistence");
+        List<Object> queryResult = session.query("getUsersForTeam", TEST_TEAM_ID);
         
         assertEquals(1, queryResult.size());
     }
     
     public void testMapProperties() throws ServiceException {
         
+        Team team = makeTestTeam();
+        List<String> aMapValue = new LinkedList<String>();
+        aMapValue.add("blub");
+        team.fooMap.put("foo", aMapValue);        
+        currentSession().persist(team);
+        
         Session session = sessionFactory_.currentSession();
         List<Object> queryResult = session.query("allTeams");
         
         assertEquals(1, queryResult.size());
         
-        Team team = (Team) queryResult.get(0);
-        assertEquals("blub", team.fooMap.get("foo").get(1));
+        Team queriedTeam = (Team) queryResult.get(0);
+        assertEquals("blub", queriedTeam.fooMap.get("foo").get(0));
     }
     
     public void testInvalidQuery() throws ServiceException {
@@ -173,75 +258,39 @@ public class JcrPersistenceTest extends JcrPersistenceTestBase {
     
     public void testQueryAllUsers() throws ServiceException {
         
+        currentSession().persist(makeTestUser());
+        
         Session session = sessionFactory_.currentSession();
         List<Object> queryResult = session.query("allUsers");
         
         assertEquals(1, queryResult.size());
     }
     
-    public void testQueryAndUpdateUser() throws ServiceException {
-        
-        Session session = sessionFactory_.currentSession();
-        List<Object> queryResult = session.query(
-                "userByLogin", "testUser");
-        
-        User user = (User) queryResult.get(0);
-        assertEquals("testUser", user.getLogin());
-        assertEquals("test", user.lastname);
-        assertEquals(2, user.getSkills().size());
-        assertEquals(2, user.getSkillsArray().length);
-        assertEquals("jcr-persistence", user.getTeams().get(0).name);
-        
-        user.lastname = "lastName";
-        user.getSkills().remove(0);
-        user.setSkillsArray(new String[] {user.getSkillsArray()[0]});
-        session.update(user);
-        
-        
-        session = sessionFactory_.currentSession();
-        queryResult = session.query(
-                "userByLogin", "testUser");
-        
-        user = (User) queryResult.get(0);
-        assertEquals("lastName", user.lastname);
-        assertEquals(1, user.getSkills().size());
-        assertEquals(1, user.getSkillsArray().length);
-    }
-    
     public void testDynamicTypedCollection() throws ServiceException {
+               
+        User user = makeTestUser();
+        currentSession().persist(user);
         
-        Session session = sessionFactory_.currentSession();
-        List<Object> queryResult = session.query(
-                "userByLogin", "testUser");
-        
-        User user = (User) queryResult.get(0);
         Group group = new Group();
         group.id = "foogroup";
         group.members.add(user);
         
-        session.persist(group);
+        currentSession().persist(group);
     }
     
-    public void testDeleteGroup() throws ServiceException {
+    public void testDeleteUser() throws Exception {
+        
+        currentSession().persist(makeTestUser());
         
         Session session = sessionFactory_.currentSession();
         List<Object> queryResult = session.query(
-                "groupById", "foogroup");
-        
-        Group group = (Group) queryResult.get(0);
-        assertEquals(1, group.members.size());
-        
-        session.delete(group);
-    }
-    
-    public void testDeleteUser() throws ServiceException {
-        
-        Session session = sessionFactory_.currentSession();
-        List<Object> queryResult = session.query(
-                "userByLogin", "testUser");
+                "userByLogin", TEST_USER_ID);
         
         User user = (User) queryResult.get(0);
-        session.delete(user.getTeams().get(0));
         session.delete(user);
+        
+        Node rootNode = getJcrSession().getRootNode();        
+        Node userParentNode = rootNode.getNode(User.PARENT_FOLDER);
+        assertFalse(userParentNode.hasNode(TEST_USER_ID));
     }
 }
