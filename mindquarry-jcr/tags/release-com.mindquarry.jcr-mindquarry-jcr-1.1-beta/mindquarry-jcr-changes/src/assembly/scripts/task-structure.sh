cc896897cf3dbdf3c1f6779012a0b692795268ed
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-05-24 14:45:41 +0000

    reverting buggy release tagging

--- mindquarry-jcr/tags/release-com.mindquarry.jcr-mindquarry-jcr-1.1-beta/mindquarry-jcr-changes/src/assembly/scripts/task-structure.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp mindquarry-migration.jar com.mindquarry.jcr.change.ChangeClient -r rmi://localhost:1099/jackrabbit -u admin -p admin -x migrate-task-structure.xsl -f teamspaces $1
--- mindquarry-jcr/tags/release-com.mindquarry.jcr-mindquarry-jcr-changes-1.1-beta/src/assembly/scripts/1.0M2-to-1.1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp mindquarry-migration.jar com.mindquarry.jcr.change.ChangeClient -r rmi://localhost:1099/jackrabbit -u admin -p admin -x migrate-1.0M2-to-1.1.xsl -f teamspaces $1
