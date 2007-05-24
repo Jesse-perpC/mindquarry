da15219dc8008d0a2d6614f23ac4116fe8850b34
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-05-24 14:08:31 +0000

    [maven-release-plugin]  copy for tag release-com.mindquarry-mindquarry-buildall-1.2

---
+++ trunk/mindquarry-base/tags/release-com.mindquarry-mindquarry-buildall-1.2/mindquarry-events/src/main/java/com/mindquarry/events/exception/UnknownEventException.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.events.exception;
+
+/**
+ * Add summary documentation here.
+ *
+ * @author <a href="mailto:your-email-address">your full name</a>
+ */
+public class UnknownEventException extends Exception {
+    /**
+     * Generated serialVersionUID.
+     */
+    private static final long serialVersionUID = -8749909927026503476L;
+    
+    public UnknownEventException(String message) {
+        super(message);
+    }
+}
