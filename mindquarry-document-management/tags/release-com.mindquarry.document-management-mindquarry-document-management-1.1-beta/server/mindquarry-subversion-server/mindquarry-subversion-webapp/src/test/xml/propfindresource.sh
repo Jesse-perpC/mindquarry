9df5b24ff5c9040340d84ab9764b384126213b44
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-05-24 14:43:42 +0000

    Removed file/folder

--- mindquarry-document-management/tags/release-com.mindquarry.document-management-mindquarry-document-management-1.1-beta/server/mindquarry-subversion-server/mindquarry-subversion-webapp/src/test/xml/propfindresource.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -v -T propfindresource.xml -H "Content-Type: text/xml" -X PROPFIND  http://localhost:8889/repo/software/test.txt | xmllint --nowarning --format -
--- mindquarry-document-management/tags/release-com.mindquarry.document-management-mindquarry-document-management-1.1-beta/server/mindquarry-subversion-server/mindquarry-subversion-webapp/src/test/xml/propfindvcc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -v -T propfindvcc.xml -H "Content-Type: text/xml" -X PROPFIND  http://localhost:8888/repos/repo1/!svn/vcc/default | xmllint --nowarning --format -
