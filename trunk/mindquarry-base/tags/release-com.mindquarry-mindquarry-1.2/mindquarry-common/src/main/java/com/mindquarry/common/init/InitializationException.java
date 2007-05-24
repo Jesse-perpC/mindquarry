1fadf8cc02394bd72d7e30dde9a730f01abb8c30
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-05-24 14:04:55 +0000

    [maven-release-plugin]  copy for tag release-com.mindquarry-mindquarry-1.2

---
+++ trunk/mindquarry-base/tags/release-com.mindquarry-mindquarry-1.2/mindquarry-common/src/main/java/com/mindquarry/common/init/InitializationException.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.common.init;
+
+/**
+ * This exception is used to indicate something went horribly wrong during
+ * initialization of a mindquarry component.
+ * 
+ * @author 
+ * <a href="bastian(dot)steinert(at)mindquarry(dot)com">Bastian Steinert</a>
+ */
+public class InitializationException extends RuntimeException {
+
+	private static final long serialVersionUID = 7476174092079563106L;
+
+	public InitializationException(String message) {
+		super(message);
+	}
+	
+	public InitializationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ trunk/mindquarry-base/tags/release-com.mindquarry-mindquarry-1.2/mindquarry-common/src/main/java/com/mindquarry/common/source/RevisableSource.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.common.source;
+
+import org.apache.excalibur.source.Source;
+
+/**
+ * A source that exists in different versions
+ * 
+ * @author 
+ * <a href="bastian(dot)steinert(at)mindquarry(dot)com">Bastian Steinert</a>
+ */
+public interface RevisableSource extends Source {
+
+    /** 
+     * Get the current revision of the source
+     */
+    String revision();
+    
+    /** 
+     * if the revision is head of all revision
+     */
+    boolean isHeadRevision();
+}
+
