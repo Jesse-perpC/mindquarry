9101815132b1be9a6302d1578a2406db81853c0b
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-05-24 14:05:46 +0000

    [maven-release-plugin]  copy for tag release-com.mindquarry.document-management.server-mindquarry-document-management-server-1.1-beta

---
+++ mindquarry-document-management/tags/release-com.mindquarry.document-management.server-mindquarry-document-management-server-1.1-beta/mindquarry-subversion-server/mindquarry-subversion-webapp/src/main/java/com/mindquarry/dma/webapp/SubversionServlet.java
@@ -0,0 +1,37 @@
+package com.mindquarry.dma.webapp;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SubversionServlet extends HttpServlet {
+	private List<RequestHandler> handlers;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		for (RequestHandler handler : handlers) {
+			try {
+				if (handler.handle(request, response)) {
+					return;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void init() throws ServletException {
+		this.handlers = new Vector<RequestHandler>();
+		this.handlers.add(new PropfindVccHandler());
+		this.handlers.add(new PropfindHandler());
+		this.handlers.add(new FallbackHandler());
+	}
+
+}
