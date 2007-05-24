e6b26d05ca5f0783c376eb30ab6fc15372977f4f
alexs <alexs@b115fd6e-e050-0410-b27a-7792256590da>
2007-05-24 14:45:41 +0000

    reverting buggy release tagging

--- trunk/mindquarry-jcr/tags/release-com.mindquarry.jcr-mindquarry-jackrabbit-1.1-beta/src/main/java/com/mindquarry/jcr/xenodot/XenodotClient.java
+++
@@ -1,9 +0,0 @@
-package com.mindquarry.jcr.xenodot;
-
-import java.sql.Connection;
-
-public interface XenodotClient {
-    public static final String ROLE = XenodotClient.class.getName();
-    
-    public Connection getConnection();
-}
--- trunk/mindquarry-jcr/tags/release-com.mindquarry.jcr-mindquarry-jcr-changes-1.1-beta/src/assembly/scripts/task-structure.bat
+++
@@ -1,1 +0,0 @@
-java -cp mindquarry-migration.jar com.mindquarry.jcr.change.ChangeClient -r rmi://localhost:1099/jackrabbit -u admin -p admin -x migrate-task-structure.xsl -f teamspaces %1
